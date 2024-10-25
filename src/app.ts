2a3cc520b92310fe1e4477da0a753498149d8d6b
markpit <mpitten@companieshouse.gov.uk>
2024-10-25 13:51:21 +0100

    LP-150 Move views folder and use njk for template names

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.set("dev", config.NODE_ENV === "development");
 
 // set up the template engine
 nunjucks.configure([
-  "views"
+  path.join(__dirname, "views")
 ], {
   autoescape: true,
   express: app,
@@ -22,7 +22,7 @@ nunjucks.configure([
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "html");
+app.set("view engine", "njk");
 
 // apply our default router to /
 app.use("/", router);
