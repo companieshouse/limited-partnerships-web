34f1c5c87eb39d83c8f226927460d2c61efcbfa3
lduranteau <lduranteau@companieshouse.gov.uk>
2024-10-30 10:50:36 +0000

    lp-160 : add layout

--- src/app.ts
+++ src/app.ts
@@ -12,15 +12,25 @@ app.set("port", config.PORT);
 app.set("dev", config.NODE_ENV === "development");
 
 // set up the template engine
-nunjucks.configure([
-  path.join(__dirname, "views")
+const nunjucksEnv = nunjucks.configure([
+  path.join(__dirname, "views"),
+  "node_modules/govuk-frontend",
+  "node_modules/govuk-frontend/components",
+  "node_modules/@companieshouse",
 ], {
   autoescape: true,
   express: app,
 });
 
+nunjucksEnv.addGlobal("CDN_HOST", config.CDN_HOST);
+nunjucksEnv.addGlobal("MATOMO_ASSET_PATH", `//${config.CDN_HOST}`);
+nunjucksEnv.addGlobal("PIWIK_SITE_ID", config.PIWIK_SITE_ID);
+nunjucksEnv.addGlobal("PIWIK_URL", config.PIWIK_URL);
+nunjucksEnv.addGlobal("SERVICE_NAME", config.SERVICE_NAME);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "njk");
 
