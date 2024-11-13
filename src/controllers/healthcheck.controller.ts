aa5c94cdaea651522fc617ca2123a2ae3619acfb
markpit <mpitten@companieshouse.gov.uk>
2024-11-13 17:22:55 +0000

    Update sonar property name for coverage and add some dummy code to be analysed

--- src/controllers/healthcheck.controller.ts
+++ src/controllers/healthcheck.controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 
 export const get = (req: Request, res: Response) => {
+  const a = [100];
+  for (let i = 0; i < a.length; i++) {
+    a[i] = i;
+  }
   res.status(200).json({ status: "OK" });
 };
