4309b4e25ca1fcaf40fe76726c32e94c32769608
markpit <mpitten@companieshouse.gov.uk>
2024-11-22 13:34:11 +0000

    Example of better way to manage flags and mocks

--- src/controllers/start.controller.ts
+++ src/controllers/start.controller.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 
 import * as config from "../config";
+import { hasFeature } from "utils/feature.flag";
 
 export const get = (_req: Request, res: Response, _next: NextFunction) => {
   // example error message using the locales json files
   const errorMessage = res.locals.i18n["startPage"]["errorStartAddressMissing"];
 
+  if (hasFeature().FLAG_1) {
+    console.log("***** FLAG_1");
+  }
+  if (hasFeature().FLAG_2) {
+    console.log("***** FLAG_2");
+  }
   return res.render(config.START_TEMPLATE, {
     errorMessage
   });
