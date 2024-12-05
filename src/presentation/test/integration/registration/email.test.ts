4024ab31f53ce09ee7a18b284155d4a5df2c7920
lduranteau <lduranteau@companieshouse.gov.uk>
2024-12-05 09:59:39 +0000

    lp-233 : remove email page title

--- src/presentation/test/integration/registration/email.test.ts
+++ src/presentation/test/integration/registration/email.test.ts
@@ -25,7 +25,6 @@ describe("Email Page", () => {
       const res = await request(app).get(EMAIL_URL + "?lang=en");
 
       expect(res.status).toBe(200);
-      expect(res.text).toContain(enTranslationText.emailPage.title);
       expect(res.text).toContain(enTranslationText.emailPage.whatIsEmail);
       expect(res.text).toContain(enTranslationText.emailPage.emailHint);
       expect(res.text).toContain(enTranslationText.buttons.saveAndContinue);
@@ -37,7 +36,6 @@ describe("Email Page", () => {
       const res = await request(app).get(EMAIL_URL + "?lang=cy");
 
       expect(res.status).toBe(200);
-      expect(res.text).toContain(cyTranslationText.emailPage.title);
       expect(res.text).toContain(cyTranslationText.emailPage.whatIsEmail);
       expect(res.text).toContain(cyTranslationText.emailPage.emailHint);
       expect(res.text).toContain(cyTranslationText.buttons.saveAndContinue);
