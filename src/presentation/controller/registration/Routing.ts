bbfc06fb989871e3d6c2a92c093131cd23d6ad31
mattch1 <mculpin@companieshouse.gov.uk>
2024-12-05 14:51:53 +0000

    LP-235 General partners page template and basic routing AC1

--- src/presentation/controller/registration/Routing.ts
+++ src/presentation/controller/registration/Routing.ts
@@ -12,11 +12,14 @@ const START_URL = `${BASE_URL}/start`;
 export const WHICH_TYPE_TEMPLATE = RegistrationPageType.whichType;
 export const NAME_TEMPLATE = RegistrationPageType.name;
 export const NEXT_TEMPLATE = RegistrationPageType.next;
+export const GENERAL_PARTNERS_TEMPLATE = RegistrationPageType.generalPartners;
 
 export const WHICH_TYPE_URL = `${BASE_URL}/${WHICH_TYPE_TEMPLATE}`;
 export const NAME_URL = `${BASE_URL}/${NAME_TEMPLATE}`;
 
 export const NEXT_URL = `${BASE_URL}/transaction/${TRANSACTION_ID}/submission/${SUBMISSION_ID}/${NEXT_TEMPLATE}`;
+
+export const GENERAL_PARTNERS_URL = `${BASE_URL}/${GENERAL_PARTNERS_TEMPLATE}`;
 
 const registrationRoutingPartnershipType = {
   previousUrl: START_URL,
@@ -39,10 +42,18 @@ const registrationRoutingNext = {
   pageType: RegistrationPageType.next,
 };
 
+const registrationRoutingGeneralPartners = {
+  previousUrl: NEXT_URL,
+  currentUrl: GENERAL_PARTNERS_URL,
+  nextUrl: NEXT_URL,
+  pageType: RegistrationPageType.generalPartners,
+};
+
 const list = [
   registrationRoutingPartnershipType,
   registrationRoutingName,
   registrationRoutingNext,
+  registrationRoutingGeneralPartners,
 ];
 
 export const registrationsRouting: PagesRouting = new Map<
