f5a8908fbd2e7fd80e70fbfa64ada70b8d01eddc
hatidev <hmutasa@companieshouse.gov.uk>
2025-01-16 15:42:01 +0000

    lp406 draft pr create-choose-registered-office-address page

--- src/presentation/controller/registration/PageType.ts
+++ src/presentation/controller/registration/PageType.ts
@@ -2,6 +2,7 @@ enum RegistrationPageType {
   whichType = "which-type",
   name = "name",
   email = "email",
+  chooseRegisteredOfficeAddress = "choose-registered-office-address",
   generalPartners = "general-partners",
   limitedPartners = "limited-partners",
   generalPartnerChoice = "general-partner-choice",
---
+++ src/presentation/test/integration/registration/choose-registered-office-address.test.ts
@@ -0,0 +1,45 @@
+import request from "supertest";
+import { LocalesService } from "@companieshouse/ch-node-utils";
+import * as config from "../../../../config/constants";
+import enTranslationText from "../../../../../locales/en/translations.json";
+import cyTranslationText from "../../../../../locales/cy/translations.json";
+import app from "../app";
+import { appDevDependencies } from "../../../../config/dev-dependencies";
+
+describe.skip("Choose Registered Office Address Page", () => {
+  beforeEach(() => {
+    setLocalesEnabled(false);
+  });
+
+  const setLocalesEnabled = (bool: boolean) => {
+    jest.spyOn(config, "isLocalesEnabled").mockReturnValue(bool);
+    LocalesService.getInstance().enabled = bool;
+  };
+
+  describe("Get Choose Registered Office Address Page", () => {
+    it.only("should load the choose registered office address page with English text", async () => {
+      setLocalesEnabled(true);
+      const res = await request(app).get("CHOOSE_REGISTERED_OFFICE_ADDRESS_URL" + "?lang=en");
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        `${enTranslationText.chooseRegisteredOfficeAddressPage.title} - ${enTranslationText.service} - GOV.UK`
+      );
+      expect(res.text).toContain(enTranslationText.chooseRegisteredOfficeAddressPage.title);
+      expect(res.text).toContain(enTranslationText.buttons.saveAndContinue);
+      expect(res.text).not.toContain("WELSH -");
+    });
+
+    it.only("should load the choose registered office address page with Welsh text", async () => {
+      setLocalesEnabled(true);
+      const res = await request(app).get("CHOOSE_REGISTERED_OFFICE_ADDRESS_URL" + "?lang=cy");
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        `${cyTranslationText.chooseRegisteredOfficeAddressPage.title} - ${cyTranslationText.service} - GOV.UK`
+      );
+      expect(res.text).toContain(cyTranslationText.chooseRegisteredOfficeAddressPage.title);
+      expect(res.text).toContain(cyTranslationText.buttons.saveAndContinue);
+    });
+  });
+});
