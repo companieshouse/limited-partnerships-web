c01378c8a70d2d5d0f1b2a1991e3c8d634ee06db
markpit <mpitten@companieshouse.gov.uk>
2025-03-06 14:34:41 +0000

    LP-301 Add test for general partner legal entity

---
+++ src/presentation/test/integration/registration/add-general-partner-legal-entity.test.ts
@@ -0,0 +1,146 @@
+import request from "supertest";
+import { createApiClient } from "@companieshouse/api-sdk-node";
+import enTranslationText from "../../../../../locales/en/translations.json";
+import cyTranslationText from "../../../../../locales/cy/translations.json";
+import app from "../app";
+import { ADD_GENERAL_PARTNER_LEGAL_ENTITY_URL, LIMITED_PARTNERS_URL } from "../../../controller/registration/url";
+import LimitedPartnershipBuilder from "../../builder/LimitedPartnershipBuilder";
+import { appDevDependencies } from "../../../../config/dev-dependencies";
+import { getUrl, setLocalesEnabled, testTranslations } from "../../utils";
+import RegistrationPageType from "../../../controller/registration/PageType";
+import { ApiErrors } from "../../../../domain/entities/UIErrors";
+import appRealDependencies from "../../../../app";
+import sdkMock from "../mock/sdkMock";
+
+jest.mock("@companieshouse/api-sdk-node");
+
+const mockCreateApiClient = createApiClient as jest.Mock;
+mockCreateApiClient.mockReturnValue(sdkMock);
+
+describe("Add General Partner Legal Entity Page", () => {
+  const URL = getUrl(ADD_GENERAL_PARTNER_LEGAL_ENTITY_URL);
+  const REDIRECT_URL = getUrl(LIMITED_PARTNERS_URL);
+
+  beforeEach(() => {
+    setLocalesEnabled(false);
+
+    appDevDependencies.generalPartnerGateway.feedGeneralPartners([]);
+    mockCreateApiClient.mockReturnValue(sdkMock);
+  });
+
+  describe("Get Add General Partner Legal Entity Page", () => {
+    it("should load the add general partner legal entity page with Welsh text", async () => {
+      setLocalesEnabled(true);
+      const res = await request(app).get(URL + "?lang=cy");
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        `${cyTranslationText.addGeneralPartnerLegalEntityPage.title} - ${cyTranslationText.service} - GOV.UK`
+      );
+      testTranslations(res.text, cyTranslationText.addGeneralPartnerLegalEntityPage);
+    });
+
+    it("should load the add general partner legal entity page with English text", async () => {
+      setLocalesEnabled(true);
+      const res = await request(app).get(URL + "?lang=en");
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        `${enTranslationText.addGeneralPartnerLegalEntityPage.title} - ${enTranslationText.service} - GOV.UK`
+      );
+      testTranslations(res.text, enTranslationText.addGeneralPartnerLegalEntityPage);
+      expect(res.text).not.toContain("WELSH -");
+    });
+
+    it("should contain the proposed name - data from api", async () => {
+      const limitedPartnership = new LimitedPartnershipBuilder().build();
+
+      appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([
+        limitedPartnership
+      ]);
+
+      const res = await request(app).get(URL);
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        `${limitedPartnership?.data?.partnership_name?.toUpperCase()} ${limitedPartnership?.data?.name_ending?.toUpperCase()}`
+      );
+    });
+  });
+
+  describe("Post Add General Partner Legal Entity", () => {
+    it("should send the general partner legal entity details", async () => {
+      const res = await request(app).post(URL).send({
+        pageType: RegistrationPageType.addGeneralPartnerLegalEntity,
+        legal_entity_name: "test"
+      });
+
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(`Redirecting to ${REDIRECT_URL}`);
+    });
+
+    it("should return a validation error when invalid data is entered", async () => {
+      const apiErrors: ApiErrors = {
+        errors: { "legal_entity_name": "Legal entity name is invalid" }
+      };
+
+      appDevDependencies.generalPartnerGateway.feedErrors(apiErrors);
+
+      const res = await request(app).post(URL).send({
+        pageType: RegistrationPageType.addGeneralPartnerLegalEntity,
+        legal_entity_name: "INVALID-CHARACTERS"
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain("Legal entity name is invalid");
+    });
+  });
+
+  describe("400", () => {
+    it("should return validation errors - add general partner legal entity page", async () => {
+      mockCreateApiClient.mockReturnValue({
+        ...sdkMock,
+        limitedPartnershipsService: {
+          ...sdkMock.limitedPartnershipsService,
+          postGeneralPartner: () => ({
+            httpStatusCode: 400,
+            resource: {
+              errors: {
+                legalEntityName: "Invalid value for legal entity name"
+              }
+            }
+          })
+        }
+      });
+
+      const res = await request(appRealDependencies).post(URL).send({
+        pageType: RegistrationPageType.addGeneralPartnerLegalEntity
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain("Invalid value for legal entity name");
+    });
+  });
+
+  describe("500", () => {
+    it("should load error page", async () => {
+      mockCreateApiClient.mockReturnValue({
+        ...sdkMock,
+        limitedPartnershipsService: {
+          ...sdkMock.limitedPartnershipsService,
+          postGeneralPartner: () => ({
+            httpStatusCode: 500,
+            resource: {}
+          })
+        }
+      });
+
+      const res = await request(appRealDependencies).post(URL).send({
+        pageType: RegistrationPageType.addGeneralPartnerLegalEntity
+      });
+
+      expect(res.status).toBe(500);
+      expect(res.text).toContain(enTranslationText.errorPage.title);
+    });
+  });
+});
