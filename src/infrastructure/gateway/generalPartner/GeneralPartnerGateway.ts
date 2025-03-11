184aea2a2d13d8077be40d96b5e927b3206b1b48
hatidev <hmutasa@companieshouse.gov.uk>
2025-03-11 10:54:29 +0000

    lp550 use correct gateway

--- src/infrastructure/gateway/generalPartner/GeneralPartnerGateway.ts
+++ src/infrastructure/gateway/generalPartner/GeneralPartnerGateway.ts
@@ -4,7 +4,7 @@ import { SDK_LIMITED_PARTNERSHIP_SERVICE } from "../../../config/constants";
 import { Resource } from "@companieshouse/api-sdk-node";
 import {
   GeneralPartner,
-  LimitedPartnershipResourceCreated
+  LimitedPartnershipResourceCreated,
 } from "@companieshouse/api-sdk-node/dist/services/limited-partnerships";
 import { convertValidDateToIsoDateString, removeEmptyStringValues } from "../utils";
 
@@ -22,21 +22,24 @@ class GeneralPartnerGateway implements IGeneralPartnerGateway {
         {
           day: data["date_of_birth-day"],
           month: data["date_of_birth-month"],
-          year: data["date_of_birth-year"]
+          year: data["date_of_birth-year"],
         },
         "date_of_birth"
       );
     }
 
     const generalPartner: GeneralPartner = { data };
-
+    console.log(transactionId);
+    console.log(generalPartner);
     const apiCall = {
       service: SDK_LIMITED_PARTNERSHIP_SERVICE,
       method: "postGeneralPartner",
-      args: [transactionId, generalPartner]
+      args: [transactionId, generalPartner],
     };
 
-    const response = await makeApiCallWithRetry<Resource<LimitedPartnershipResourceCreated>>(opt, apiCall);
+    const response = await makeApiCallWithRetry<
+      Resource<LimitedPartnershipResourceCreated>
+    >(opt, apiCall);
 
     const uiErrors = checkForBadRequest<LimitedPartnershipResourceCreated>(response);
     if (uiErrors) {
