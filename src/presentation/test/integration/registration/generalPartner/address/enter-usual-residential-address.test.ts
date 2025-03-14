edcd3d66605b58c3a2625fe7b3075a2dcf82a4f6
vschot <vschot@companieshouse.gov.uk>
2025-03-14 15:27:40 +0000

    LP-553 test in progress

---
+++ src/presentation/test/integration/registration/generalPartner/address/enter-usual-residential-address.test.ts
@@ -0,0 +1,106 @@
+import request from "supertest";
+
+import enTranslationText from "../../../../../../../locales/en/translations.json";
+import cyTranslationText from "../../../../../../../locales/cy/translations.json";
+
+import app from "../../../app";
+
+import { ENTER_USUAL_RESIDENTIAL_ADDRESS_URL } from "../../../../../controller/addressLookUp/url";
+import { LIMITED_PARTNERS_URL } from "../../../../../controller/registration/url";
+import { getUrl, setLocalesEnabled, testTranslations } from "../../../../utils";
+import AddressPageType from "../../../../../controller/addressLookUp/PageType";
+import { appDevDependencies } from "../../../../../../config/dev-dependencies";
+import GeneralPartnerBuilder, {
+  generalPartnerLegalEntity,
+  generalPartnerPerson
+} from "../../../../builder/GeneralPartnerBuilder";
+
+describe("Enter Principal Place Of Business Manual Address Page", () => {
+  const URL = getUrl(ENTER_USUAL_RESIDENTIAL_ADDRESS_URL);
+  const redirectUrl = getUrl(LIMITED_PARTNERS_URL);
+
+  beforeEach(() => {
+    setLocalesEnabled(false);
+
+    appDevDependencies.cacheRepository.feedCache(null);
+  });
+
+  describe("GET Enter general partners usual residential address", () => {
+    it("should load enter general partners usual residential address page with english text", async () => {
+      setLocalesEnabled(true);
+      const generalPartner = new GeneralPartnerBuilder()
+        .withId(appDevDependencies.generalPartnerGateway.generalPartnerId)
+        .isPerson()
+        .build();
+
+      const res = await request(app).get(URL + "?lang=en");
+
+      expect(res.status).toBe(200);
+      testTranslations(res.text, enTranslationText.address.enterAddress, [
+        "registeredOfficeAddress",
+        "principalPlaceOfBusinessAddress",
+        "jurisdictionCountry"
+      ]);
+      expect(res.text).not.toContain("WELSH -");
+      expect(res.text).toContain(generalPartner.data?.forename?.toUpperCase());
+      expect(res.text).toContain(generalPartner.data?.surname?.toUpperCase());
+      expect(res.text).not.toContain(generalPartnerLegalEntity.legal_entity_name?.toUpperCase());
+    });
+
+    it("should load enter general partners usual residential address manual entry page with welsh text", async () => {
+      setLocalesEnabled(true);
+
+      const generalPartner = new GeneralPartnerBuilder()
+        .withId(appDevDependencies.generalPartnerGateway.generalPartnerId)
+        .isPerson()
+        .build();
+
+      const res = await request(app).get(URL + "?lang=cy");
+
+      expect(res.status).toBe(200);
+      testTranslations(res.text, cyTranslationText.address.enterAddress, [
+        "registeredOfficeAddress",
+        "principalPlaceOfBusinessAddress",
+        "jurisdictionCountry"
+      ]);
+      expect(res.text).toContain(generalPartner.data?.legal_entity_name?.toUpperCase());
+      expect(res.text).not.toContain(generalPartnerPerson.forename?.toUpperCase());
+    });
+  });
+
+  describe("POST Enter general partners usual residential address Page", () => {
+    it("should redirect to the confirm general partners usual residential address page", async () => {
+      const generalPartner = new GeneralPartnerBuilder()
+        .withId(appDevDependencies.generalPartnerGateway.generalPartnerId)
+        .isPerson()
+        .build();
+
+      appDevDependencies.generalPartnerGateway.feedGeneralPartners([generalPartner]);
+
+      const res = await request(app).post(URL).send({
+        pageType: AddressPageType.enterUsualResidentialAddress,
+        country: "GB-WLS"
+      });
+
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(`Redirecting to ${redirectUrl}`);
+    });
+
+    it("should redirect to the error page when error occurs during Post", async () => {
+      const generalPartner = new GeneralPartnerBuilder()
+        .withId(appDevDependencies.generalPartnerGateway.generalPartnerId)
+        .isPerson()
+        .build();
+
+      appDevDependencies.generalPartnerGateway.feedGeneralPartners([generalPartner]);
+
+      const res = await request(app).post(URL).send({
+        pageType: "Invalid page type",
+        country: "GB-SCT"
+      });
+
+      expect(res.status).toBe(500);
+      expect(res.text).toContain(enTranslationText.errorPage.title);
+    });
+  });
+});
