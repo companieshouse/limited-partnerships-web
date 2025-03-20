739bb571209d41a7204ae60bf3d626e3ddd211dc
vschot <vschot@companieshouse.gov.uk>
2025-03-20 11:32:05 +0000

    LP-553 updated tests

--- src/presentation/test/integration/registration/generalPartner/address/enter-usual-residential-address.test.ts
+++ src/presentation/test/integration/registration/generalPartner/address/enter-usual-residential-address.test.ts
@@ -5,6 +5,7 @@ import cyTranslationText from "../../../../../../../locales/cy/translations.json
 
 import app from "../../../app";
 
+import * as config from "config";
 import { ENTER_USUAL_RESIDENTIAL_ADDRESS_URL } from "../../../../../controller/addressLookUp/url";
 import { LIMITED_PARTNERS_URL } from "../../../../../controller/registration/url";
 import { getUrl, setLocalesEnabled, testTranslations } from "../../../../utils";
@@ -14,6 +15,7 @@ import GeneralPartnerBuilder, {
   generalPartnerLegalEntity,
   generalPartnerPerson
 } from "../../../../builder/GeneralPartnerBuilder";
+import { ApiErrors } from "../../../../../../domain/entities/UIErrors";
 
 describe("Enter Usual Residential Address Page", () => {
   const URL = getUrl(ENTER_USUAL_RESIDENTIAL_ADDRESS_URL);
@@ -41,7 +43,8 @@ describe("Enter Usual Residential Address Page", () => {
         "principalPlaceOfBusinessAddress",
         "jurisdictionCountry",
         "postcodeMissing",
-        "postcodeLength"
+        "postcodeLength",
+
       ]);
       expect(res.text).not.toContain("WELSH -");
       expect(res.text).toContain(generalPartner.data?.forename?.toUpperCase());
@@ -65,33 +68,90 @@ describe("Enter Usual Residential Address Page", () => {
         "principalPlaceOfBusinessAddress",
         "jurisdictionCountry",
         "postcodeMissing",
-        "postcodeLength"
+        "postcodeLength",
+        "postcode"
       ]);
       expect(res.text).toContain(generalPartner.data?.legal_entity_name?.toUpperCase());
       expect(res.text).not.toContain(generalPartnerPerson.forename?.toUpperCase());
     });
 
-    it("should redirect to postcode lookup page when country selected is within the UK", async () => {
-      setLocalesEnabled(true);
+    it("should load enter general partners usual residential address manual entry page with overseas back link", async () => {
+      appDevDependencies.cacheRepository.feedCache({ territory_choice: "overseas" });
+      appDevDependencies.cacheRepository.feedCache({
+        [appDevDependencies.transactionGateway.transactionId]: {
+          "territory_choice": "overseas"
+        }
+      });
 
-      const generalPartner = new GeneralPartnerBuilder()
-        .withId(appDevDependencies.generalPartnerGateway.generalPartnerId)
-        .isPerson()
-        .build();
-
-      appDevDependencies.generalPartnerGateway.feedGeneralPartners([
-        generalPartner
-      ]);
-
-      const res = await request(app).get(URL + "?lang=en");
-
+      const res = await request(app).get(URL);
       expect(res.status).toBe(200);
-      const regex = new RegExp(`/limited-partnerships/transaction/.*?/submission/.*?/${redirectUrl}`);
-      expect(res.text).toMatch(regex);
+
+      expect(res.text).toContain('/general-partner-usual-residential-address-territory-choice');
+
+    });
+
+    it("should load enter general partners usual residential address manual entry page with postcode lookup back link", async () => {
+      appDevDependencies.cacheRepository.feedCache({ territory_choice: "overseas" });
+      appDevDependencies.cacheRepository.feedCache({
+        [appDevDependencies.transactionGateway.transactionId]: {
+          "territory_choice": "unitedKingdom"
+        }
+      });
+
+      const res = await request(app).get(URL);
+      expect(res.status).toBe(200);
+
+      expect(res.text).toContain('/postcode-usual-residential-address');
+
     });
   });
 
   describe("POST Enter general partners usual residential address Page", () => {
+    it("should redirect and add entered address to the cache", async () => {
+      appDevDependencies.addressLookUpGateway.setError(false);
+      appDevDependencies.cacheRepository.feedCache({
+        [appDevDependencies.transactionGateway.transactionId]: {
+          ["usual_residential_address"]: {
+            address_line_1: "",
+            address_line_2: "",
+            country: "",
+            locality: "",
+            postal_code: "",
+            premises: "",
+            region: ""
+          }
+        }
+      });
+      const res = await request(app)
+        .post(URL)
+        .send({
+          pageType: AddressPageType.enterUsualResidentialAddress,
+          postal_code: "",
+          premises: "4",
+          address_line_1: "DUNCALF STREET",
+          address_line_2: "",
+          locality: "STOKE-ON-TRENT",
+          country: "GB-ENG",
+        });
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(`Redirecting to ${redirectUrl}`);
+
+      const cache = appDevDependencies.cacheRepository.cache;
+      expect(cache?.[`${config.APPLICATION_CACHE_KEY}`]).toEqual({
+        [appDevDependencies.transactionGateway.transactionId]: {
+          usual_residential_address: {
+            postal_code: "",
+            premises: "4",
+            address_line_1: "DUNCALF STREET",
+            address_line_2: "",
+            locality: "STOKE-ON-TRENT",
+            country: "GB-ENG",
+            region: undefined
+          }
+        }
+      });
+    });
+
     it("should redirect to the confirm general partners usual residential address page", async () => {
       const generalPartner = new GeneralPartnerBuilder()
         .withId(appDevDependencies.generalPartnerGateway.generalPartnerId)
@@ -124,6 +184,30 @@ describe("Enter Usual Residential Address Page", () => {
 
       expect(res.status).toBe(500);
       expect(res.text).toContain(enTranslationText.errorPage.title);
+    });
+
+    it("should redirect if postcode is null", async () => {
+      const generalPartner = new GeneralPartnerBuilder()
+        .withId(appDevDependencies.generalPartnerGateway.generalPartnerId)
+        .build();
+
+      appDevDependencies.generalPartnerGateway.feedGeneralPartners([generalPartner]);
+
+      const apiErrors: ApiErrors = {
+        errors: {
+          "usualResidentialAddress.postalCode": "must not be null"
+        }
+      };
+
+      appDevDependencies.generalPartnerGateway.feedErrors(apiErrors);
+
+      const res = await request(app).post(URL).send({
+        pageType: AddressPageType.confirmRegisteredOfficeAddress,
+        address: `{"postal_code": "","premises": "4","address_line_1": "DUNCALF STREET","address_line_2": "","locality": "STOKE-ON-TRENT","country": "GB-ENG"}`
+      });
+
+      expect(res.status).toBe(302);
+      expect(res.text).not.toContain("must not be null");
     });
   });
 });
