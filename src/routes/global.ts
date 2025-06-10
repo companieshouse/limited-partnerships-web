9c1a67bdc443a1bba0728d2b5613042a81ff606f
mwestacott <mwestacott2@companieshouse.gov.uk>
2025-06-10 12:12:07 +0100

    LP-856 New handling for failed payments
    * Payment failed screen added
    * Support to suppress the 'Back' link in the header
    * Check payment status and route to confirmation screen
    or new payment failed screen
    * Layout changes for when 'Back' link not present
    * Tests added

--- src/routes/global.ts
+++ src/routes/global.ts
@@ -5,6 +5,8 @@ import { authentication } from "../middlewares";
 import {
   HEALTHCHECK_URL,
   SIGN_OUT_URL,
+  PAYMENT_RESPONSE_URL,
+  PAYMENT_FAILED_URL,
   CONFIRMATION_URL,
 } from "../presentation/controller/global/url";
 
@@ -27,9 +29,21 @@ export const globalEndpoints = (
   );
 
   router.get(
+    PAYMENT_RESPONSE_URL,
+    authentication,
+    dependencies.globalController.getPaymentDecision()
+  );
+
+  router.get(
     CONFIRMATION_URL,
     authentication,
     dependencies.globalController.getConfirmationPage()
+  );
+
+  router.get(
+    PAYMENT_FAILED_URL,
+    authentication,
+    dependencies.globalController.getPageRouting()
   );
 };
 
