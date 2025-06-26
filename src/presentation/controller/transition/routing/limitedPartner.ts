f21a55160e5dda225f4c8251a1a9a7ef25e149c8
lduranteau <lduranteau@companieshouse.gov.uk>
2025-06-26 12:06:17 +0100

    lp-933 : fix typo

--- src/presentation/controller/transition/routing/limitedPartner.ts
+++ src/presentation/controller/transition/routing/limitedPartner.ts
@@ -1,25 +1,25 @@
-import TransitionnPageType from "../PageType";
+import TransitionPageType from "../PageType";
 import * as url from "../url";
 
 const transitionRoutingLimitedPartners = {
   previousUrl: url.REVIEW_GENERAL_PARTNERS_URL,
   currentUrl: url.LIMITED_PARTNERS_URL,
   nextUrl: url.LIMITED_PARTNER_CHOICE_URL,
-  pageType: TransitionnPageType.limitedPartners
+  pageType: TransitionPageType.limitedPartners
 };
 
 const transitionRoutingLimitedPartnerChoice = {
   previousUrl: url.LIMITED_PARTNERS_URL,
   currentUrl: url.LIMITED_PARTNER_CHOICE_URL,
   nextUrl: "",
-  pageType: TransitionnPageType.limitedPartnerChoice
+  pageType: TransitionPageType.limitedPartnerChoice
 };
 
 const transitionRoutingAddLimitedPartnerPerson = {
   previousUrl: url.LIMITED_PARTNER_CHOICE_URL,
   currentUrl: url.ADD_LIMITED_PARTNER_PERSON_URL,
   nextUrl: "/", // TODO set to TERRITORY_CHOICE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL
-  pageType: TransitionnPageType.addLimitedPartnerPerson,
+  pageType: TransitionPageType.addLimitedPartnerPerson,
   data: {
     customPreviousUrl: "/" // TODO set to REVIEW_LIMITED_PARTNERS_URL
   }
@@ -29,7 +29,7 @@ const transitionRoutingAddLimitedPartnerLegalEntity = {
   previousUrl: url.LIMITED_PARTNER_CHOICE_URL,
   currentUrl: url.ADD_LIMITED_PARTNER_LEGAL_ENTITY_URL,
   nextUrl: "/", // TODO set to TERRITORY_CHOICE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL
-  pageType: TransitionnPageType.addLimitedPartnerLegalEntity,
+  pageType: TransitionPageType.addLimitedPartnerLegalEntity,
   data: {
     customPreviousUrl: url.REVIEW_LIMITED_PARTNERS_URL
   }
