0fde2e77924f488070f10f6eff9f8143635ddb91
markpit <mpitten@companieshouse.gov.uk>
2025-08-08 12:00:06 +0100

    LP-953 add companyAuthentication middleware to post transition address routes

--- src/routes/addressPostTransition.ts
+++ src/routes/addressPostTransition.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { authentication } from "../middlewares";
+import { authentication, companyAuthentication } from "../middlewares";
 
 import { IDependencies } from "../config/IDependencies";
 
@@ -14,55 +14,65 @@ export const addressLookUpEndpoints = (router: Router, dependencies: IDependenci
   router.get(
     url.TERRITORY_CHOICE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.TERRITORY_CHOICE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.handleTerritoryChoice()
   );
 
   router.get(
     url.POSTCODE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.POSTCODE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.postcodeValidation()
   );
 
   router.get(
     url.CHOOSE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.CHOOSE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.selectAddress()
   );
 
   router.get(
     url.ENTER_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.ENTER_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.sendManualAddress()
   );
 
   router.get(
     url.CONFIRM_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.CONFIRM_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.confirmAddress()
   );
 
@@ -70,55 +80,65 @@ export const addressLookUpEndpoints = (router: Router, dependencies: IDependenci
   router.get(
     url.TERRITORY_CHOICE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.TERRITORY_CHOICE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.handleTerritoryChoice()
   );
 
   router.get(
     url.POSTCODE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.POSTCODE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.postcodeValidation()
   );
 
   router.get(
     url.CHOOSE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.CHOOSE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.selectAddress()
   );
 
   router.get(
     url.ENTER_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.ENTER_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.sendManualAddress()
   );
 
   router.get(
     url.CONFIRM_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.CONFIRM_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.confirmAddress()
   );
 
@@ -126,55 +146,65 @@ export const addressLookUpEndpoints = (router: Router, dependencies: IDependenci
   router.get(
     url.TERRITORY_CHOICE_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.TERRITORY_CHOICE_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.handleTerritoryChoice()
   );
 
   router.get(
     url.POSTCODE_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.POSTCODE_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.postcodeValidation()
   );
 
   router.get(
     url.CHOOSE_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.CHOOSE_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.selectAddress()
   );
 
   router.get(
     url.ENTER_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.ENTER_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.sendManualAddress()
   );
 
   router.get(
     url.CONFIRM_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.getPageRouting()
   );
   router.post(
     url.CONFIRM_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
     authentication,
+    companyAuthentication,
     dependencies.addressLookUpController.confirmAddress()
   );
 };
