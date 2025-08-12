68f74d43accb31aae7cb28d86c8ef46313f12b77
lduranteau <lduranteau@companieshouse.gov.uk>
2025-08-12 11:39:46 +0100

    lp-949 : fix company name in page that contains errors

--- src/presentation/controller/common/GeneralPartnerController.ts
+++ src/presentation/controller/common/GeneralPartnerController.ts
@@ -77,7 +77,7 @@ abstract class GeneralPartnerController extends AbstractController {
     };
   }
 
-  private async getLimitedPartnershipDetails(tokens: Tokens, companyId: string): Promise<Record<string, any>> {
+  protected async getLimitedPartnershipDetails(tokens: Tokens, companyId: string): Promise<Record<string, any>> {
     const result = await (this.companyService as CompanyService).getCompanyProfile(tokens, companyId);
 
     return {
--- src/presentation/controller/postTransition/GeneralPartnerController.ts
+++ src/presentation/controller/postTransition/GeneralPartnerController.ts
@@ -65,12 +65,12 @@ class GeneralPartnerPostTransitionController extends GeneralPartnerController {
           kind: isLegalEntity ? PartnerKind.ADD_GENERAL_PARTNER_LEGAL_ENTITY : PartnerKind.ADD_GENERAL_PARTNER_PERSON
         });
 
-        if (result.errors && companyResult) {
-          const companyProfile = companyResult.companyProfile;
+        if (result.errors) {
+          const limitedPartnership = await this.getLimitedPartnershipDetails(tokens, ids.companyId);
 
           response.render(
             super.templateName(pageRouting.currentUrl),
-            super.makeProps(pageRouting, { companyProfile, generalPartner: { data: request.body } }, result.errors)
+            super.makeProps(pageRouting, { limitedPartnership, generalPartner: { data: request.body } }, result.errors)
           );
 
           return;
