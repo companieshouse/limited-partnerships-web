23ffbba063f1d16bd02c4de2cbe4adc1117bf573
mattch1 <mculpin@companieshouse.gov.uk>
2025-08-12 15:21:07 +0100

    Further test fixes

--- src/presentation/test/integration/postTransition/generalPartner/general-partner-check-your-answers-person.test.ts
+++ src/presentation/test/integration/postTransition/generalPartner/general-partner-check-your-answers-person.test.ts
@@ -8,80 +8,77 @@ import { appDevDependencies } from "../../../../../config/dev-dependencies";
 import GeneralPartnerBuilder from "../../../builder/GeneralPartnerBuilder";
 import CompanyProfileBuilder from "../../../builder/CompanyProfileBuilder";
 import { GENERAL_PARTNER_CHECK_YOUR_ANSWERS_URL } from "../../../../controller/postTransition/url";
-import { getUrl, setLocalesEnabled, testTranslations } from "../../../utils";
+import { getUrl, setLocalesEnabled } from "../../../utils";
 import { formatDate } from "../../../../../utils/date-format";
 import { GeneralPartner } from "@companieshouse/api-sdk-node/dist/services/limited-partnerships";
 import { CONFIRM_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL, CONFIRM_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL } from "../../../../controller/addressLookUp/url/registration";
 
 describe("General Partner Check Your Answers Page", () => {
-    const URL = getUrl(GENERAL_PARTNER_CHECK_YOUR_ANSWERS_URL);
-    
-    let generalPartnerPerson;
-    
-    beforeEach(() => {
-      const companyProfile = new CompanyProfileBuilder().build();
-      appDevDependencies.companyGateway.feedCompanyProfile(companyProfile.data);
-/*
-      appDevDependencies.cacheRepository.feedCache({
-        [APPLICATION_CACHE_KEY_COMPANY_NUMBER]: companyProfile.data.companyNumber
-      });*/
-      generalPartnerPerson = new GeneralPartnerBuilder().isPerson().withFormerNames("Joe Dee").withDateEffectiveFrom("2024-10-10").build();
-      appDevDependencies.generalPartnerGateway.feedGeneralPartners([generalPartnerPerson]);
-    });
+  const URL = getUrl(GENERAL_PARTNER_CHECK_YOUR_ANSWERS_URL);
 
-    it("should GET Check Your Answers Page English text", async () => {
+  let generalPartnerPerson;
+
+  beforeEach(() => {
+    const companyProfile = new CompanyProfileBuilder().build();
+    appDevDependencies.companyGateway.feedCompanyProfile(companyProfile.data);
+
+    generalPartnerPerson = new GeneralPartnerBuilder().isPerson().withFormerNames("Joe Dee").withDateEffectiveFrom("2024-10-10").build();
+    appDevDependencies.generalPartnerGateway.feedGeneralPartners([generalPartnerPerson]);
+  });
+
+  it("should GET Check Your Answers Page English text", async () => {
+    setLocalesEnabled(true);
+    const res = await request(app).get(URL + "?lang=en");
+
+    expect(res.status).toBe(200);
+
+    expect(res.text).toContain(enTranslationText.generalPartnercheckYourAnswersPage.title);
+    expect(res.text).toContain(enTranslationText.print.buttonText);
+    expect(res.text).toContain(enTranslationText.print.buttonTextNoJs);
+    expect(res.text).not.toContain("WELSH -");
+  });
+
+  it("should GET Check Your Answers Page Welsh text", async () => {
+    setLocalesEnabled(true);
+    const res = await request(app).get(URL + "?lang=cy");
+
+    expect(res.status).toBe(200);
+
+    expect(res.text).toContain(cyTranslationText.generalPartnercheckYourAnswersPage.title);
+    expect(res.text).toContain(cyTranslationText.print.buttonText);
+    expect(res.text).toContain(cyTranslationText.print.buttonTextNoJs);
+    expect(res.text).toContain("WELSH -");
+  });
+
+  it.each([
+    [URL + "?lang=en", "/limited-partnerships/sign-out?lang=en"],
+    [URL + "?lang=cy", "/limited-partnerships/sign-out?lang=cy"],
+    [URL, "/limited-partnerships/sign-out"]
+  ])(
+    "should set the signout link href correctly for url: %s",
+    async (testUrl: string, expectedHref: string) => {
       setLocalesEnabled(true);
-      const res = await request(app).get(URL + "?lang=en");
+      const res = await request(app).get(testUrl);
 
       expect(res.status).toBe(200);
-     
-      expect(res.text).toContain(enTranslationText.generalPartnercheckYourAnswersPage.title);
-      expect(res.text).toContain(enTranslationText.print.buttonText);
-      expect(res.text).toContain(enTranslationText.print.buttonTextNoJs);
-      expect(res.text).not.toContain("WELSH -");
-    });
+      expect(res.text).toContain(expectedHref);
+    }
+  );
 
-    it("should GET Check Your Answers Page Welsh text", async () => {
-      setLocalesEnabled(true);
-      const res = await request(app).get(URL + "?lang=cy");
+  it.skip("Should contain a back link to the confirm correspondence address page", async () => {
+    const res = await request(app).get(URL);
 
-      expect(res.status).toBe(200);
-     
-      expect(res.text).toContain(cyTranslationText.generalPartnercheckYourAnswersPage.title);
-      expect(res.text).toContain(cyTranslationText.print.buttonText);
-      expect(res.text).toContain(cyTranslationText.print.buttonTextNoJs);
-      expect(res.text).toContain("WELSH -");
-    });
+    expect(res.status).toBe(200);
+    expect(res.text).not.toContain(getUrl(CONFIRM_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL));
+    expect(res.text).toContain(getUrl(CONFIRM_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL));
+  });
 
-    it.each([
-      [URL + "?lang=en", "/limited-partnerships/sign-out?lang=en"],
-      [URL + "?lang=cy", "/limited-partnerships/sign-out?lang=cy"],
-      [URL, "/limited-partnerships/sign-out"]
-    ])(
-      "should set the signout link href correctly for url: %s",
-      async (testUrl: string, expectedHref: string) => {
-        setLocalesEnabled(true);
-        const res = await request(app).get(testUrl);
+  it("should load the check your answers page with partners - EN", async () => {
+    const res = await request(app).get(URL);
 
-        expect(res.status).toBe(200);
-        expect(res.text).toContain(expectedHref);
-      }
-    );
-
-    it.skip("Should contain a back link to the confirm correspondence address page", async () => {
-      const res = await request(app).get(URL);
-
-      expect(res.status).toBe(200);
-      expect(res.text).not.toContain(getUrl(CONFIRM_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL));
-      expect(res.text).toContain(getUrl(CONFIRM_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL));
-    });
-
-    it("should load the check your answers page with partners - EN", async () => {
-      const res = await request(app).get(URL);
-
-      expect(res.status).toBe(200);
-      checkIfValuesInText(res, generalPartnerPerson, enTranslationText);
-    });
+    expect(res.status).toBe(200);
+    checkIfValuesInText(res, generalPartnerPerson, enTranslationText);
+  });
 });
 
 const checkIfValuesInText = (res: request.Response, partner: GeneralPartner, translationText: Record<string, any>) => {
@@ -92,7 +89,7 @@ const checkIfValuesInText = (res: request.Response, partner: GeneralPartner, tra
 
         expect(res.text).toContain(capitalized);
       } else if (key.includes("date_of_birth") && partner.data[key]) {
-        //expect(res.text).toContain(formatDate(partner.data[key], translationText));
+        expect(res.text).toContain(formatDate(partner.data[key], translationText));
       } else if (key.includes("usual_residential_address")) {
         const capitalized = partner.data[key].address_line_1
           .split(" ")
@@ -100,8 +97,8 @@ const checkIfValuesInText = (res: request.Response, partner: GeneralPartner, tra
           .join(" ");
         expect(res.text).toContain(capitalized);
       } else if (key.includes("date_effective_from")) {
-        //expect(res.text).toContain(formatDate(partner.data[key], translationText));
-      } 
+        expect(res.text).toContain(formatDate(partner.data[key], translationText));
+      }
     }
   }
 };
