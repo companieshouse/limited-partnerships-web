36e1f4246698db14478a9700b287e7d714d9d820
lduranteau <lduranteau@companieshouse.gov.uk>
2025-08-14 14:21:12 +0100

    lp977 : post-transition - lp ura address

---
+++ src/presentation/test/integration/postTransition/limitedPartner/addresses/confirm-limited-partner-usual-residential-address.test.ts
@@ -0,0 +1,161 @@
+import request from "supertest";
+
+import app from "../../../app";
+import enTranslationText from "../../../../../../../locales/en/translations.json";
+import cyTranslationText from "../../../../../../../locales/cy/translations.json";
+
+import { getUrl, setLocalesEnabled, testTranslations } from "../../../../utils";
+import {
+  CONFIRM_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
+  ENTER_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
+  POSTCODE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL
+} from "../../../../../controller/addressLookUp/url/postTransition";
+import { appDevDependencies } from "../../../../../../config/dev-dependencies";
+import AddressPageType from "../../../../../controller/addressLookUp/PageType";
+import { ApiErrors } from "../../../../../../domain/entities/UIErrors";
+import LimitedPartnerBuilder from "../../../../builder/LimitedPartnerBuilder";
+
+describe("Confirm Limited Partner Usual Residential Address Page", () => {
+  const URL = getUrl(CONFIRM_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL);
+
+  beforeEach(() => {
+    setLocalesEnabled(false);
+    appDevDependencies.cacheRepository.feedCache({
+      [appDevDependencies.transactionGateway.transactionId]: {
+        ["usual_residential_address"]: {
+          postal_code: "ST6 3LJ",
+          premises: "4",
+          address_line_1: "line 1",
+          address_line_2: "line 2",
+          locality: "stoke-on-trent",
+          region: "region",
+          country: "England"
+        }
+      }
+    });
+
+    const limitedPartner = new LimitedPartnerBuilder()
+      .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+      .isPerson()
+      .build();
+
+    appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartner]);
+    appDevDependencies.limitedPartnerGateway.feedErrors();
+  });
+
+  describe("GET Confirm Usual Residential Address Page", () => {
+    it("should load the confirm usual residential address page with English text", async () => {
+      setLocalesEnabled(true);
+
+      const res = await request(app).get(URL + "?lang=en");
+
+      expect(res.status).toBe(200);
+      testTranslations(res.text, enTranslationText.address.confirm.limitedPartnerUsualResidentialAddress);
+      expect(res.text).not.toContain("WELSH -");
+
+      expect(res.text).toContain("4 Line 1");
+      expect(res.text).toContain("Line 2");
+      expect(res.text).toContain("Stoke-On-Trent");
+      expect(res.text).toContain("Region");
+      expect(res.text).toContain("England");
+      expect(res.text).toContain("ST6 3LJ");
+    });
+
+    it("should load the confirm usual residential address page with Welsh text", async () => {
+      setLocalesEnabled(true);
+
+      const res = await request(app).get(URL + "?lang=cy");
+
+      expect(res.status).toBe(200);
+      testTranslations(res.text, cyTranslationText.address.confirm.limitedPartnerUsualResidentialAddress);
+
+      expect(res.text).toContain("4 Line 1");
+      expect(res.text).toContain("Line 2");
+      expect(res.text).toContain("Stoke-On-Trent");
+      expect(res.text).toContain("Region");
+      expect(res.text).toContain("England");
+      expect(res.text).toContain("ST6 3LJ");
+    });
+
+    it.each([
+      ["overseas", getUrl(ENTER_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL)],
+      ["unitedKingdom", getUrl(POSTCODE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL)]
+    ])("should have the correct back link", async (territory, backLink) => {
+      appDevDependencies.cacheRepository.feedCache({
+        [appDevDependencies.transactionGateway.transactionId]: {
+          ura_territory_choice: territory
+        }
+      });
+
+      const res = await request(app).get(URL);
+
+      expect(res.text).toContain(backLink);
+    });
+  });
+
+  describe("POST Confirm Usual Residential Address Page", () => {
+    it("should redirect to the next page", async () => {
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isPerson()
+        .build();
+
+      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartner]);
+
+      const res = await request(app)
+        .post(URL)
+        .send({
+          pageType: AddressPageType.confirmLimitedPartnerUsualResidentialAddress,
+          address: `{
+            "postal_code": "ST6 3LJ",
+            "premises": "4",
+            "address_line_1": "DUNCALF STREET",
+            "address_line_2": "",
+            "locality": "STOKE-ON-TRENT",
+            "country": "England"
+          }`
+        });
+
+      const redirectUrl = getUrl(""); // TODO - redirect to check your answer page
+
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(`Redirecting to ${redirectUrl}`);
+    });
+
+    it("should show error message if address is not provided", async () => {
+      appDevDependencies.cacheRepository.feedCache({});
+
+      const res = await request(app).post(URL).send({
+        pageType: AddressPageType.confirmLimitedPartnerUsualResidentialAddress
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain("You must provide an address");
+    });
+
+    it("should show validation error message if validation error occurs when saving address", async () => {
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isPerson()
+        .build();
+
+      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartner]);
+
+      const apiErrors: ApiErrors = {
+        errors: {
+          "usualResidentialAddress.country": "must not be null"
+        }
+      };
+
+      appDevDependencies.limitedPartnerGateway.feedErrors(apiErrors);
+
+      const res = await request(app).post(URL).send({
+        pageType: AddressPageType.confirmLimitedPartnerUsualResidentialAddress,
+        address: `{"postal_code": "ST6 3LJ","premises": "4","address_line_1": "DUNCALF STREET","address_line_2": "","locality": "STOKE-ON-TRENT","country": ""}`
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain("must not be null");
+    });
+  });
+});
---
+++ src/presentation/test/integration/postTransition/limitedPartner/addresses/territory-choice-limited-partner-usual-residential-address.test.ts
@@ -0,0 +1,110 @@
+import request from "supertest";
+import enTranslationText from "../../../../../../../locales/en/translations.json";
+import cyTranslationText from "../../../../../../../locales/cy/translations.json";
+import app from "../../../app";
+import { appDevDependencies } from "../../../../../../config/dev-dependencies";
+import { getUrl, setLocalesEnabled, testTranslations, toEscapedHtml } from "../../../../utils";
+import {
+  ENTER_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
+  TERRITORY_CHOICE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
+  POSTCODE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL
+} from "../../../../../controller/addressLookUp/url/postTransition";
+import AddressPageType from "../../../../../controller/addressLookUp/PageType";
+import LimitedPartnerBuilder, { limitedPartnerPerson } from "../../../../builder/LimitedPartnerBuilder";
+import { APPLICATION_CACHE_KEY } from "../../../../../../config/constants";
+
+describe("Limited Partner Usual Residential Address Territory Choice", () => {
+  const URL = getUrl(TERRITORY_CHOICE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL);
+
+  beforeEach(() => {
+    setLocalesEnabled(false);
+    appDevDependencies.limitedPartnerGateway.feedLimitedPartners([]);
+  });
+
+  describe("GET /limited-partner-usual-residential-address-choose-territory", () => {
+    it("should load the limited partner usual residential address territory choice page with Welsh text", async () => {
+      setLocalesEnabled(true);
+      const res = await request(app).get(URL + "?lang=cy");
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        toEscapedHtml(
+          `${cyTranslationText.address.territoryChoice.limitedPartnerUsualResidentialAddress.title} - ${cyTranslationText.servicePostTransition} - GOV.UK`
+        )
+      );
+
+      testTranslations(res.text, cyTranslationText.address.territoryChoice.limitedPartnerUsualResidentialAddress);
+      testTranslations(res.text, cyTranslationText.address.territories);
+    });
+
+    it("should load the limited partner usual residential address territory choice page with English text", async () => {
+      setLocalesEnabled(true);
+      const res = await request(app).get(URL + "?lang=en");
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        toEscapedHtml(
+          `${enTranslationText.address.territoryChoice.limitedPartnerUsualResidentialAddress.title} - ${enTranslationText.servicePostTransition} - GOV.UK`
+        )
+      );
+
+      testTranslations(res.text, enTranslationText.address.territories);
+    });
+
+    it("should contain the limited partner name - data from API", async () => {
+      const limitedPartner = new LimitedPartnerBuilder()
+        .isPerson()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .build();
+
+      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartner]);
+
+      const res = await request(app).get(URL);
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        `${limitedPartnerPerson.forename.toUpperCase()} ${limitedPartnerPerson.surname.toUpperCase()}`
+      );
+    });
+  });
+
+  describe("POST /limited-partner-territory-choice", () => {
+    it("should redirect to What is the limited partners URA? post code look up page when united kingdom is selected", async () => {
+      const UNITED_KINGDOM_PARAMETER = "unitedKingdom";
+      const POSTCODE_URL = getUrl(POSTCODE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL);
+      const res = await request(app).post(URL).send({
+        pageType: AddressPageType.territoryChoiceLimitedPartnerUsualResidentialAddress,
+        parameter: UNITED_KINGDOM_PARAMETER
+      });
+
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(POSTCODE_URL);
+      expect(appDevDependencies.cacheRepository.cache).toEqual({
+        [APPLICATION_CACHE_KEY]: {
+          [appDevDependencies.transactionGateway.transactionId]: {
+            ura_territory_choice: "unitedKingdom"
+          }
+        }
+      });
+    });
+
+    it("should redirect to What is the limited partners URA? manual entry page when overseas is selected", async () => {
+      const OVERSEAS_PARAMETER = "overseas";
+      const MANUAL_ENTRY_URL = getUrl(ENTER_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL);
+      const res = await request(app).post(URL).send({
+        pageType: AddressPageType.territoryChoiceLimitedPartnerUsualResidentialAddress,
+        parameter: OVERSEAS_PARAMETER
+      });
+
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(MANUAL_ENTRY_URL);
+      expect(appDevDependencies.cacheRepository.cache).toEqual({
+        [APPLICATION_CACHE_KEY]: {
+          [appDevDependencies.transactionGateway.transactionId]: {
+            ura_territory_choice: "overseas"
+          }
+        }
+      });
+    });
+  });
+});
