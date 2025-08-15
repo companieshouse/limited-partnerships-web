ddf31d79cefc23d5e7907dc6344ddaf9ff6b85d7
mattch1 <mculpin@companieshouse.gov.uk>
2025-08-15 09:31:54 +0100

    LP-978 PT principal office address loop

--- src/presentation/controller/addressLookUp/routing/postTransition/limitedPartner.ts
+++ src/presentation/controller/addressLookUp/routing/postTransition/limitedPartner.ts
@@ -1,4 +1,4 @@
-import { ADD_LIMITED_PARTNER_PERSON_WITH_IDS_URL } from "../../../postTransition/url";
+import { ADD_LIMITED_PARTNER_LEGAL_ENTITY_WITH_IDS_URL, ADD_LIMITED_PARTNER_PERSON_WITH_IDS_URL, LIMITED_PARTNER_CHECK_YOUR_ANSWERS_URL } from "../../../postTransition/url";
 import AddressPageType from "../../PageType";
 import * as url from "../../url/postTransition";
 
@@ -78,6 +78,77 @@ const usualResidentialAddress = [
   postTransitionAddressRoutingConfirmLimitedPartnerUsualResidentialAddress
 ];
 
-const limitedPartnerRouting = [...usualResidentialAddress];
+// principal office address - legal entity
+
+const principalOfficeAddressCacheKeys = {
+  [AddressCacheKeys.addressCacheKey]: "principal_office_address",
+  [AddressCacheKeys.territoryCacheKey]: "poa_territory_choice"
+};
+
+const postTransitionAddressRoutingTerritoryChoiceLimitedPartnerPrincipalOfficeAddress = {
+  previousUrl: ADD_LIMITED_PARTNER_LEGAL_ENTITY_WITH_IDS_URL,
+  currentUrl: url.TERRITORY_CHOICE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  nextUrl: url.POSTCODE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  pageType: AddressPageType.territoryChoiceLimitedPartnerPrincipalOfficeAddress,
+  data: {
+    ...principalOfficeAddressCacheKeys,
+    nextUrlOverseas: url.ENTER_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL
+  }
+};
+
+const postTransitionAddressRoutingPostcodeLimitedPartnerPrincipalOfficeAddress = {
+  previousUrl: url.TERRITORY_CHOICE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  currentUrl: url.POSTCODE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  nextUrl: url.CHOOSE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  pageType: AddressPageType.postcodeLimitedPartnerPrincipalOfficeAddress,
+  data: {
+    ...principalOfficeAddressCacheKeys,
+    enterManualAddressPageType: AddressPageType.enterLimitedPartnerPrincipalOfficeAddress,
+    confirmAddressUrl: url.CONFIRM_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL
+  }
+};
+
+const postTransitionAddressRoutingChooseLimitedPartnerPrincipalOfficeAddress = {
+  previousUrl: url.POSTCODE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  currentUrl: url.CHOOSE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  nextUrl: url.CONFIRM_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  pageType: AddressPageType.chooseLimitedPartnerPrincipalOfficeAddress,
+  data: {
+    ...principalOfficeAddressCacheKeys,
+    enterManualAddressPageType: AddressPageType.enterLimitedPartnerPrincipalOfficeAddress
+  }
+};
+
+const postTransitionAddressRoutingEnterLimitedPartnerPrincipalOfficeAddress = {
+  previousUrl: url.POSTCODE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  currentUrl: url.ENTER_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  nextUrl: url.CONFIRM_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  pageType: AddressPageType.enterLimitedPartnerPrincipalOfficeAddress,
+  data: {
+    ...principalOfficeAddressCacheKeys,
+    territoryPageType: AddressPageType.territoryChoiceLimitedPartnerPrincipalOfficeAddress
+  }
+};
+
+const postTransitionAddressRoutingConfirmLimitedPartnerPrincipalOfficeAddress = {
+  previousUrl: url.POSTCODE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  currentUrl: url.CONFIRM_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  nextUrl: LIMITED_PARTNER_CHECK_YOUR_ANSWERS_URL,
+  pageType: AddressPageType.confirmLimitedPartnerPrincipalOfficeAddress,
+  data: {
+    ...principalOfficeAddressCacheKeys,
+    enterManualAddressPageType: AddressPageType.enterLimitedPartnerPrincipalOfficeAddress
+  }
+};
+
+const principalOfficeAddress = [
+  postTransitionAddressRoutingTerritoryChoiceLimitedPartnerPrincipalOfficeAddress,
+  postTransitionAddressRoutingPostcodeLimitedPartnerPrincipalOfficeAddress,
+  postTransitionAddressRoutingEnterLimitedPartnerPrincipalOfficeAddress,
+  postTransitionAddressRoutingChooseLimitedPartnerPrincipalOfficeAddress,
+  postTransitionAddressRoutingConfirmLimitedPartnerPrincipalOfficeAddress
+];
+
+const limitedPartnerRouting = [...usualResidentialAddress, ...principalOfficeAddress];
 
 export default limitedPartnerRouting;
---
+++ src/presentation/test/integration/postTransition/limitedPartner/address/enter-limited-partner-principal-office-address.test.ts
@@ -0,0 +1,247 @@
+import request from "supertest";
+
+import enTranslationText from "../../../../../../../locales/en/translations.json";
+import cyTranslationText from "../../../../../../../locales/cy/translations.json";
+
+import app from "../../../app";
+import { getUrl, setLocalesEnabled, testTranslations } from "../../../../utils";
+import { appDevDependencies } from "../../../../../../config/dev-dependencies";
+
+import {
+  ENTER_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+  CONFIRM_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL
+} from "presentation/controller/addressLookUp/url/postTransition";
+import LimitedPartnerBuilder, {
+  limitedPartnerLegalEntity,
+  limitedPartnerPerson
+} from "../../../../builder/LimitedPartnerBuilder";
+import AddressPageType from "../../../../../controller/addressLookUp/PageType";
+
+describe("Enter limited partner's principal office manual address page", () => {
+  const URL = getUrl(ENTER_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL);
+
+  beforeEach(() => {
+    setLocalesEnabled(false);
+    appDevDependencies.cacheRepository.feedCache(null);
+    appDevDependencies.limitedPartnerGateway.feedLimitedPartners([]);
+  });
+
+  describe("Get enter limited partner's principal office address page", () => {
+    it("should load enter limited partners principal office address page with English text", async () => {
+      setLocalesEnabled(true);
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isLegalEntity()
+        .build();
+
+      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartner]);
+
+      const res = await request(app).get(URL + "?lang=en");
+
+      expect(res.status).toBe(200);
+      testTranslations(res.text, enTranslationText.address.enterAddress, [
+        "registeredOfficeAddress",
+        "principalPlaceOfBusinessAddress",
+        "jurisdictionCountry",
+        "postcodeMissing",
+        "postcodeLength",
+        "usualResidentialAddress",
+        "correspondenceAddress",
+        "principalOfficeAddress",
+        "errorMessages"
+      ]);
+      expect(res.text).not.toContain("WELSH -");
+      expect(res.text).not.toContain(limitedPartnerPerson.forename?.toUpperCase());
+      expect(res.text).not.toContain(limitedPartnerPerson.surname?.toUpperCase());
+      expect(res.text).toContain(limitedPartnerLegalEntity.legal_entity_name?.toUpperCase());
+    });
+
+    it("should load enter limited partners principal office address page with Welsh text", async () => {
+      setLocalesEnabled(true);
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isLegalEntity()
+        .build();
+
+      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartner]);
+
+      const res = await request(app).get(URL + "?lang=cy");
+
+      expect(res.status).toBe(200);
+      testTranslations(res.text, cyTranslationText.address.enterAddress, [
+        "registeredOfficeAddress",
+        "principalPlaceOfBusinessAddress",
+        "jurisdictionCountry",
+        "postcodeMissing",
+        "postcodeLength",
+        "usualResidentialAddress",
+        "correspondenceAddress",
+        "principalOfficeAddress",
+        "errorMessages"
+      ]);
+      expect(res.text).toContain("WELSH -");
+      expect(res.text).not.toContain(limitedPartnerPerson.forename?.toUpperCase());
+      expect(res.text).not.toContain(limitedPartnerPerson.surname?.toUpperCase());
+      expect(res.text).toContain(limitedPartnerLegalEntity.legal_entity_name?.toUpperCase());
+    });
+  });
+
+  describe("Post enter limited partner's principal office address page", () => {
+    it("should redirect to the error page when error occurs during Post", async () => {
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isLegalEntity()
+        .build();
+
+      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartner]);
+
+      const res = await request(app).post(URL).send({
+        pageType: "Invalid page type",
+        country: ""
+      });
+
+      expect(res.status).toBe(500);
+      expect(res.text).toContain(enTranslationText.errorPage.title);
+    });
+
+    it("should not return a validation error when an overseas address and postcode does not conform to UK format", async () => {
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isPerson()
+        .build();
+
+      const res = await request(app)
+        .post(URL)
+        .send({
+          pageType: AddressPageType.enterLimitedPartnerPrincipalOfficeAddress,
+          ...limitedPartner.data?.principal_office_address,
+          postal_code: "here",
+          country: "Vatican City"
+        });
+
+      const redirectUrl = getUrl(CONFIRM_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL);
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(`Redirecting to ${redirectUrl}`);
+    });
+
+    it("should return a validation error when a UK address and postcode format is invalid", async () => {
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isPerson()
+        .build();
+
+      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartner]);
+
+      const res = await request(app)
+        .post(URL)
+        .send({
+          pageType: AddressPageType.enterLimitedPartnerPrincipalOfficeAddress,
+          ...limitedPartner.data?.principal_office_address,
+          postal_code: "here"
+        });
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(enTranslationText.address.enterAddress.errorMessages.postcodeFormat);
+      expect(res.text).toContain(enTranslationText.govUk.error.title);
+      expect(res.text).toContain(
+        `${limitedPartner.data?.forename?.toUpperCase()} ${limitedPartner.data?.surname?.toUpperCase()}`
+      );
+    });
+
+    it("should not return validation errors when address fields contain valid but non alpha-numeric characters", async () => {
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isPerson()
+        .build();
+
+      const res = await request(app)
+        .post(URL)
+        .send({
+          pageType: AddressPageType.enterLimitedPartnerPrincipalOfficeAddress,
+          ...limitedPartner.data?.principal_office_address,
+          premises: "-,.:; &@$£¥€'?!/\\řśŝşšţťŧùúûüũūŭůűųŵẁẃẅỳýŷÿźżžñńņňŋòóôõöøōŏőǿœŕŗàáâãäåāăąæǽçćĉċč",
+          address_line_1: "()[]{}<>*=#%+ÀÁÂÃÄÅĀĂĄÆǼÇĆĈĊČÞĎÐÈÉÊËĒĔĖĘĚĜĞĠĢ",
+          address_line_2: "ĤĦÌÍÎÏĨĪĬĮİĴĶĹĻĽĿŁÑŃŅŇŊÒÓÔÕÖØŌŎŐǾŒŔŖŘŚŜŞŠŢŤŦ",
+          locality: "ÙÚÛÜŨŪŬŮŰŲŴẀẂẄỲÝŶŸŹŻŽa-zÀÖØſƒǺẀỲ",
+          region: "þďðèéêëēĕėęěĝģğġĥħìíîïĩīĭįĵķĺļľŀł"
+        });
+
+      const redirectUrl = getUrl(CONFIRM_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL);
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(`Redirecting to ${redirectUrl}`);
+    });
+
+    it("should return validation errors when address fields contain invalid characters", async () => {
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isPerson()
+        .build();
+
+      const res = await request(app)
+        .post(URL)
+        .send({
+          pageType: AddressPageType.enterLimitedPartnerPrincipalOfficeAddress,
+          ...limitedPartner.data?.principal_office_address,
+          premises: "±",
+          address_line_1: "±",
+          address_line_2: "±",
+          locality: "±",
+          region: "±"
+        });
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        enTranslationText.address.enterAddress.premises +
+          " " +
+          enTranslationText.address.enterAddress.errorMessages.invalidCharacters
+      );
+      expect(res.text).toContain(
+        enTranslationText.address.enterAddress.addressLine1 +
+          " " +
+          enTranslationText.address.enterAddress.errorMessages.invalidCharacters
+      );
+      expect(res.text).toContain(
+        enTranslationText.address.enterAddress.addressLine2Title +
+          " " +
+          enTranslationText.address.enterAddress.errorMessages.invalidCharacters
+      );
+      expect(res.text).toContain(
+        enTranslationText.address.enterAddress.locality +
+          " " +
+          enTranslationText.address.enterAddress.errorMessages.invalidCharacters
+      );
+      expect(res.text).toContain(
+        enTranslationText.address.enterAddress.regionTitle +
+          " " +
+          enTranslationText.address.enterAddress.errorMessages.invalidCharacters
+      );
+      expect(res.text).toContain(enTranslationText.govUk.error.title);
+    });
+
+    it("should return validation errors when address fields exceed character limit", async () => {
+      const limitedPartner = new LimitedPartnerBuilder()
+        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
+        .isPerson()
+        .build();
+
+      const res = await request(app)
+        .post(URL)
+        .send({
+          pageType: AddressPageType.enterLimitedPartnerPrincipalOfficeAddress,
+          ...limitedPartner.data?.principal_office_address,
+          premises: "toomanycharacters".repeat(13),
+          address_line_1: "toomanycharacters".repeat(4),
+          address_line_2: "toomanycharacters".repeat(4),
+          locality: "toomanycharacters".repeat(4),
+          region: "toomanycharacters".repeat(4)
+        });
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(enTranslationText.address.enterAddress.errorMessages.premisesLength);
+      expect(res.text).toContain(enTranslationText.address.enterAddress.errorMessages.addressLine1Length);
+      expect(res.text).toContain(enTranslationText.address.enterAddress.errorMessages.addressLine2Length);
+      expect(res.text).toContain(enTranslationText.address.enterAddress.errorMessages.localityLength);
+      expect(res.text).toContain(enTranslationText.address.enterAddress.errorMessages.regionLength);
+    });
+  });
+});
