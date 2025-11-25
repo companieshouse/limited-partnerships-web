ba13a08993a190963e284f2c5b1ac08bdc64746e
lduranteau <lduranteau@companieshouse.gov.uk>
2025-11-25 09:41:40 +0000

    lp-1378 : update url uk-or-overseas-*

--- src/presentation/controller/addressLookUp/PageType.ts
+++ src/presentation/controller/addressLookUp/PageType.ts
@@ -16,21 +16,21 @@ enum AddressPageType {
   // GENERAL PARTNER
 
   // usual residential address - Person
-  territoryChoiceGeneralPartnerUsualResidentialAddress = "territory-choice-general-partner-usual-residential-address",
+  territoryChoiceGeneralPartnerUsualResidentialAddress = "uk-or-overseas-general-partner-usual-residential-address",
   postcodeGeneralPartnerUsualResidentialAddress = "postcode-general-partner-usual-residential-address",
   chooseGeneralPartnerUsualResidentialAddress = "choose-general-partner-usual-residential-address",
   enterGeneralPartnerUsualResidentialAddress = "enter-general-partner-usual-residential-address",
   confirmGeneralPartnerUsualResidentialAddress = "confirm-general-partner-usual-residential-address",
 
   // correspondence address - Person
-  territoryChoiceGeneralPartnerCorrespondenceAddress = "territory-choice-general-partner-correspondence-address",
+  territoryChoiceGeneralPartnerCorrespondenceAddress = "uk-or-overseas-general-partner-correspondence-address",
   postcodeGeneralPartnerCorrespondenceAddress = "postcode-general-partner-correspondence-address",
   chooseGeneralPartnerCorrespondenceAddress = "choose-general-partner-correspondence-address",
   enterGeneralPartnerCorrespondenceAddress = "enter-general-partner-correspondence-address",
   confirmGeneralPartnerCorrespondenceAddress = "confirm-general-partner-correspondence-address",
 
   // principal office address - Legal Entity
-  territoryChoiceGeneralPartnerPrincipalOfficeAddress = "territory-choice-general-partner-principal-office-address",
+  territoryChoiceGeneralPartnerPrincipalOfficeAddress = "uk-or-overseas-general-partner-principal-office-address",
   postcodeGeneralPartnerPrincipalOfficeAddress = "postcode-general-partner-principal-office-address",
   chooseGeneralPartnerPrincipalOfficeAddress = "choose-general-partner-principal-office-address",
   enterGeneralPartnerPrincipalOfficeAddress = "enter-general-partner-principal-office-address",
@@ -39,17 +39,18 @@ enum AddressPageType {
   // LIMITED PARTNER
 
   // usual residential address - Person
-  territoryChoiceLimitedPartnerUsualResidentialAddress = "territory-choice-limited-partner-usual-residential-address",
+  territoryChoiceLimitedPartnerUsualResidentialAddress = "uk-or-overseas-limited-partner-usual-residential-address",
   postcodeLimitedPartnerUsualResidentialAddress = "postcode-limited-partner-usual-residential-address",
   chooseLimitedPartnerUsualResidentialAddress = "choose-limited-partner-usual-residential-address",
   enterLimitedPartnerUsualResidentialAddress = "enter-limited-partner-usual-residential-address",
   confirmLimitedPartnerUsualResidentialAddress = "confirm-limited-partner-usual-residential-address",
 
   // principal office address - Legal entity
-  territoryChoiceLimitedPartnerPrincipalOfficeAddress = "territory-choice-limited-partner-principal-office-address",
+  territoryChoiceLimitedPartnerPrincipalOfficeAddress = "uk-or-overseas-limited-partner-principal-office-address",
   postcodeLimitedPartnerPrincipalOfficeAddress = "postcode-limited-partner-principal-office-address",
   chooseLimitedPartnerPrincipalOfficeAddress = "choose-limited-partner-principal-office-address",
   enterLimitedPartnerPrincipalOfficeAddress = "enter-limited-partner-principal-office-address",
   confirmLimitedPartnerPrincipalOfficeAddress = "confirm-limited-partner-principal-office-address"
 }
+
 export default AddressPageType;
