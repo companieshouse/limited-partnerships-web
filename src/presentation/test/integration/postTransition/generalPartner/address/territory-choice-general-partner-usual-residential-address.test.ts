506c56f85189c0217b873130b8be259bb87b7f43
markpit <mpitten@companieshouse.gov.uk>
2025-12-15 10:52:02 +0000

    LP-1468 Add new test to check correct back link for add and update person

--- src/presentation/test/integration/postTransition/generalPartner/address/territory-choice-general-partner-usual-residential-address.test.ts
+++ src/presentation/test/integration/postTransition/generalPartner/address/territory-choice-general-partner-usual-residential-address.test.ts
@@ -12,6 +12,8 @@ import {
 import AddressPageType from "../../../../../controller/addressLookUp/PageType";
 import GeneralPartnerBuilder, { generalPartnerPerson } from "../../../../builder/GeneralPartnerBuilder";
 import { APPLICATION_CACHE_KEY } from "../../../../../../config/constants";
+import { PartnerKind } from "@companieshouse/api-sdk-node/dist/services/limited-partnerships";
+import { ADD_GENERAL_PARTNER_PERSON_WITH_IDS_URL, UPDATE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_YES_NO_URL } from "../../../../../../presentation/controller/postTransition/url";
 
 describe("General Partner Usual Residential Address Territory Choice", () => {
   const URL = getUrl(TERRITORY_CHOICE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL);
@@ -64,6 +66,26 @@ describe("General Partner Usual Residential Address Territory Choice", () => {
       expect(res.status).toBe(200);
       expect(res.text).toContain(
         `${generalPartnerPerson.forename.toUpperCase()} ${generalPartnerPerson.surname.toUpperCase()}`
+      );
+    });
+
+    it.each([
+      ["update", PartnerKind.UPDATE_GENERAL_PARTNER_PERSON, UPDATE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_YES_NO_URL],
+      ["add", PartnerKind.ADD_GENERAL_PARTNER_PERSON, ADD_GENERAL_PARTNER_PERSON_WITH_IDS_URL]
+    ])("should contain the correct back link when on %s general partner person journey", async (_description: string, partnerKind: PartnerKind, backUrl: string) => {
+      const generalPartner = new GeneralPartnerBuilder()
+        .isPerson()
+        .withId(appDevDependencies.generalPartnerGateway.generalPartnerId)
+        .withKind(partnerKind)
+        .build();
+
+      appDevDependencies.generalPartnerGateway.feedGeneralPartners([generalPartner]);
+
+      const res = await request(app).get(URL);
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain(
+        getUrl(backUrl)
       );
     });
   });
