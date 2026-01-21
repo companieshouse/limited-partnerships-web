da35e6d604c2d00982750666934e64cc74044cb0
lduranteau <lduranteau@companieshouse.gov.uk>
2026-01-21 13:57:39 +0000

    lp-1566 : remove unused builder method - sq

--- src/presentation/test/builder/FilingHistoryBuilder.ts
+++ src/presentation/test/builder/FilingHistoryBuilder.ts
@@ -1,40 +1,15 @@
 import { FilingHistoryItem } from "@companieshouse/api-sdk-node/dist/services/company-filing-history/types";
 
 class FilingHistoryBuilder {
-  private transactionId = "NGA0NDcwMzcyMWFkaXF6a2N4";
-  private barcode = "8G3ZMW4B";
   private type = "LPTS01";
-  private date = "2025-08-06";
-  private category = "incorporation";
-  private description = "transition-of-limited-partnership";
-
-  withTransactionId(transactionId: string): this {
-    this.transactionId = transactionId;
-    return this;
-  }
-
-  withBarcode(barcode: string): this {
-    this.barcode = barcode;
-    return this;
-  }
+  private readonly transactionId = "NGA0NDcwMzcyMWFkaXF6a2N4";
+  private readonly barcode = "8G3ZMW4B";
+  private readonly date = "2025-08-06";
+  private readonly category = "incorporation";
+  private readonly description = "transition-of-limited-partnership";
 
   withType(type: string): this {
     this.type = type;
-    return this;
-  }
-
-  withDate(date: string): this {
-    this.date = date;
-    return this;
-  }
-
-  withCategory(category: string): this {
-    this.category = category;
-    return this;
-  }
-
-  withDescription(description: string): this {
-    this.description = description;
     return this;
   }
 
