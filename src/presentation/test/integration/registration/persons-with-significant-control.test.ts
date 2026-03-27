a83a3ae900571e3a500b9f3d561b8d3f6d195db5
vschot <vschot@companieshouse.gov.uk>
2026-03-27 15:51:31 +0000

    translation + update tests
    new translations + structure
    update tests to use new structure

---
+++ src/presentation/test/integration/registration/persons-with-significant-control.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+import enTranslationText from "../../../../../locales/en/translations.json";
+import cyTranslationText from "../../../../../locales/cy/translations.json";
+import app from "../app";
+import { TELL_US_ABOUT_PSC_URL, WILL_LIMITED_PARTNERSHIP_HAVE_PSC_URL } from "../../../controller/registration/url";
+import LimitedPartnershipBuilder from "../../builder/LimitedPartnershipBuilder";
+import { appDevDependencies } from "../../../../config/dev-dependencies";
+import { getUrl, setLocalesEnabled, testTranslations } from "../../utils";
+
+describe("Tell Us About PSC Page", () => {
+  const URL = getUrl(TELL_US_ABOUT_PSC_URL);
+  const REDIRECT_URL = getUrl(WILL_LIMITED_PARTNERSHIP_HAVE_PSC_URL);
+  let limitedPartnership;
+
+  beforeEach(() => {
+    setLocalesEnabled(true);
+
+    limitedPartnership = new LimitedPartnershipBuilder().build();
+    appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
+  });
+
+  it.each([
+    "en",
+    "cy"
+  ])("should load the Tell Us About PSC page with %s text", async (lang: string) => {
+    const translationText = lang === "en" ? enTranslationText : cyTranslationText;
+
+    const res = await request(app).get(URL + `?lang=${lang}`);
+
+    expect(res.status).toBe(200);
+    expect(res.text).toContain(
+      `${translationText.personWithSignificantControl.tellUsAboutPscPage.title} - ${translationText.serviceRegistration} - GOV.UK`
+    );
+    expect(res.text).toContain(
+      `${limitedPartnership?.data?.partnership_name?.toUpperCase()} ${limitedPartnership?.data?.name_ending?.toUpperCase()}`
+    );
+    expect(res.text).toContain(REDIRECT_URL);
+    testTranslations(res.text, translationText.personWithSignificantControl.tellUsAboutPscPage);
+  });
+});
