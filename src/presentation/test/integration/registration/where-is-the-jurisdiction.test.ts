480ae8d3c677f2bb864051599326209599335248
vschot <vschot@companieshouse.gov.uk>
2026-05-12 15:29:16 +0100

    updated and added new tests for jurisdiction

--- src/presentation/test/integration/registration/where-is-the-jurisdiction.test.ts
+++ src/presentation/test/integration/registration/where-is-the-jurisdiction.test.ts
@@ -1,122 +1,97 @@
 import request from "supertest";
-import { PartnershipType } from "@companieshouse/api-sdk-node/dist/services/limited-partnerships";
+import { Jurisdiction, PartnershipType } from "@companieshouse/api-sdk-node/dist/services/limited-partnerships";
 
 import enTranslationText from "../../../../../locales/en/translations.json";
 import cyTranslationText from "../../../../../locales/cy/translations.json";
+import enErrorMessages from "../../../../../locales/en/errors.json";
+import cyErrorMessages from "../../../../../locales/cy/errors.json";
 import app from "../app";
-import { getUrl, setLocalesEnabled, testTranslations } from "../../utils";
+import { countOccurrences, getUrl, setLocalesEnabled, testTranslations } from "../../utils";
 import { appDevDependencies } from "../../../../config/dev-dependencies";
 import { WHERE_IS_THE_JURISDICTION_URL } from "../../../controller/registration/url";
 import LimitedPartnershipBuilder from "../../builder/LimitedPartnershipBuilder";
+import RegistrationPageType from "../../../controller/registration/PageType";
+import { POSTCODE_REGISTERED_OFFICE_ADDRESS_URL } from "../../../controller/addressLookUp/url/registration";
 
 describe("Where is the jurisdiction page", () => {
   const URL = getUrl(WHERE_IS_THE_JURISDICTION_URL);
+  const REDIRECT_URL = getUrl(POSTCODE_REGISTERED_OFFICE_ADDRESS_URL);
 
   beforeEach(() => {
-    setLocalesEnabled(false);
+    setLocalesEnabled(true);
+
+    appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([]);
   });
 
   describe("GET Where is the jurisdiction page", () => {
-    it("should load the jurisdiction page with English text for EnglandWales LP jurisdiction", async () => {
-      setLocalesEnabled(true);
-
-      const limitedPartnership = new LimitedPartnershipBuilder().withPartnershipType(PartnershipType.LP).build();
+    it.each([
+      ["en", PartnershipType.LP, "EnglandWales"],
+      ["cy", PartnershipType.LP, "EnglandWales"],
+      ["en", PartnershipType.PFLP, "EnglandWales"],
+      ["cy", PartnershipType.PFLP, "EnglandWales"],
+      ["en", PartnershipType.SLP, "Scotland"],
+      ["cy", PartnershipType.SLP, "Scotland"],
+      ["en", PartnershipType.SPFLP, "Scotland"],
+      ["cy", PartnershipType.SPFLP, "Scotland"]
+    ])("should load the jurisdiction page with %s text for %s %s jurisdiction", async (lang: string, partnershipType: PartnershipType, jurisdiction: string) => {
+      const translationText = lang === "en" ? enTranslationText : cyTranslationText;
+      const limitedPartnership = new LimitedPartnershipBuilder().withPartnershipType(partnershipType).build();
 
       appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
 
-      const res = await request(app).get(URL + "?lang=en");
+      const res = await request(app).get(URL + `?lang=${lang}`);
 
       expect(res.status).toBe(200);
-      testTranslations(res.text, enTranslationText.whereIsTheJurisdiction, ["scotland"]);
-      expect(res.text).toContain(
-        `${enTranslationText.whereIsTheJurisdiction.title} - ${enTranslationText.serviceRegistration} - GOV.UK`
-      );
-      expect(res.text).toContain(enTranslationText.buttons.saveAndContinue);
+      if (jurisdiction === "Scotland") {
+        expect(res.text).toContain(
+          `${translationText.whereIsTheJurisdiction.scotland.title} - ${translationText.serviceRegistration} - GOV.UK`
+        );
+        testTranslations(res.text, translationText.whereIsTheJurisdiction.scotland, []);
+      } else {
+        expect(res.text).toContain(
+          `${translationText.whereIsTheJurisdiction.title} - ${translationText.serviceRegistration} - GOV.UK`
+        );
+        testTranslations(res.text, translationText.whereIsTheJurisdiction, ["scotland"]);
+      }
+      expect(res.text).toContain(translationText.buttons.saveAndContinue);
+    });
+  });
+
+  describe("POST Where is the jurisdiction page", () => {
+    it.each([
+      [PartnershipType.LP, Jurisdiction.ENGLAND_AND_WALES],
+      [PartnershipType.PFLP, Jurisdiction.ENGLAND_AND_WALES],
+      [PartnershipType.LP, Jurisdiction.NORTHERN_IRELAND],
+      [PartnershipType.PFLP, Jurisdiction.NORTHERN_IRELAND]
+    ])("should redirect to the postcode registered office address page for partnership type %s when %s jurisdiction is selected", async (partnershipType: PartnershipType, jurisdiction: Jurisdiction) => {
+      const limitedPartnership = new LimitedPartnershipBuilder().withPartnershipType(partnershipType).build();
+
+      appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
+
+      const res = await request(app).post(URL).send({
+        pageType: RegistrationPageType.jurisdiction,
+        jurisdiction: jurisdiction
+      });
+
+      expect(res.status).toBe(302);
+      expect(res.header.location).toBe(REDIRECT_URL);
     });
 
-    it("should load the jurisdiction page with Welsh text for EnglandWales LP jurisdiction", async () => {
-      setLocalesEnabled(true);
-
-      const limitedPartnership = new LimitedPartnershipBuilder().withPartnershipType(PartnershipType.LP).build();
-
-      appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
-
-      const res = await request(app).get(URL + "?lang=cy");
-
-      expect(res.status).toBe(200);
-      expect(res.text).toContain(
-        `${cyTranslationText.whereIsTheJurisdiction.title} - ${cyTranslationText.serviceRegistration} - GOV.UK`
-      );
-      testTranslations(res.text, cyTranslationText.whereIsTheJurisdiction, ["scotland"]);
-      expect(res.text).toContain(cyTranslationText.buttons.saveAndContinue);
-    });
-
-    it("should load the jurisdiction page with English text for EnglandWales PFLP jurisdiction", async () => {
-      setLocalesEnabled(true);
-
-      const limitedPartnership = new LimitedPartnershipBuilder().withPartnershipType(PartnershipType.PFLP).build();
+    it.each([
+      ["en", PartnershipType.LP],
+      ["cy", PartnershipType.LP],
+      ["en", PartnershipType.PFLP],
+      ["cy", PartnershipType.PFLP],
+    ])("should return validation error when no jurisdiction is selected", async (lang: string, partnershipType: PartnershipType) => {
+      const translationText = lang === "en" ? enErrorMessages : cyErrorMessages;
+      const limitedPartnership = new LimitedPartnershipBuilder().withPartnershipName(partnershipType).build();
 
       appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
 
-      const res = await request(app).get(URL + "?lang=en");
+      const res = await request(app).post(URL + `?lang=${lang}`).send({ pageType: RegistrationPageType.jurisdiction });
 
       expect(res.status).toBe(200);
-      expect(res.text).toContain(
-        `${enTranslationText.whereIsTheJurisdiction.title} - ${enTranslationText.serviceRegistration} - GOV.UK`
-      );
-      testTranslations(res.text, enTranslationText.whereIsTheJurisdiction, ["scotland"]);
-      expect(res.text).toContain(enTranslationText.buttons.saveAndContinue);
-    });
-
-    it("should load the jurisdiction page with Welsh text for EnglandWales PFLP jurisdiction", async () => {
-      setLocalesEnabled(true);
-
-      const limitedPartnership = new LimitedPartnershipBuilder().withPartnershipType(PartnershipType.PFLP).build();
-
-      appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
-
-      const res = await request(app).get(URL + "?lang=cy");
-
-      expect(res.status).toBe(200);
-      expect(res.text).toContain(
-        `${cyTranslationText.whereIsTheJurisdiction.title} - ${cyTranslationText.serviceRegistration} - GOV.UK`
-      );
-      testTranslations(res.text, cyTranslationText.whereIsTheJurisdiction, ["scotland"]);
-      expect(res.text).toContain(cyTranslationText.buttons.saveAndContinue);
-    });
-
-    it("should load the jurisdiction page with English text for Scotland SLP jurisdiction", async () => {
-      setLocalesEnabled(false);
-
-      const limitedPartnership = new LimitedPartnershipBuilder().withPartnershipType(PartnershipType.SLP).build();
-
-      appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
-
-      const res = await request(app).get(URL);
-
-      expect(res.status).toBe(200);
-      expect(res.text).toContain(
-        `${enTranslationText.whereIsTheJurisdiction.scotland.title} - ${enTranslationText.serviceRegistration} - GOV.UK`
-      );
-      testTranslations(res.text, enTranslationText.whereIsTheJurisdiction.scotland);
-      expect(res.text).toContain(enTranslationText.buttons.saveAndContinue);
-    });
-
-    it("should load the jurisdiction page with English text for Scotland SPFLP jurisdiction", async () => {
-      setLocalesEnabled(false);
-
-      const limitedPartnership = new LimitedPartnershipBuilder().withPartnershipType(PartnershipType.SPFLP).build();
-
-      appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
-
-      const res = await request(app).get(URL);
-
-      expect(res.status).toBe(200);
-      expect(res.text).toContain(
-        `${enTranslationText.whereIsTheJurisdiction.scotland.title} - ${enTranslationText.serviceRegistration} - GOV.UK`
-      );
-      testTranslations(res.text, enTranslationText.whereIsTheJurisdiction.scotland);
-      expect(res.text).toContain(enTranslationText.buttons.saveAndContinue);
+      expect(countOccurrences(res.text, translationText.errorMessages.limitedPartnership.jurisdiction.required)).toBe(2);
     });
   });
 });
