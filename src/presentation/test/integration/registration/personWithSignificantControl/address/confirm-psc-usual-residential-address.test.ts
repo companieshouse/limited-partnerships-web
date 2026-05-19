cfb573263efc005ce3173f9ed1515be2d68bffc8
vschot <vschot@companieshouse.gov.uk>
2026-05-19 17:13:54 +0100

    tests for psc ura

---
+++ src/presentation/test/integration/registration/personWithSignificantControl/address/confirm-psc-usual-residential-address.test.ts
@@ -0,0 +1,196 @@
+import request from "supertest";
+
+import enGeneralTranslationText from "../../../../../../../locales/en/translations.json";
+import cyGeneralTranslationText from "../../../../../../../locales/cy/translations.json";
+import enAddressTranslationText from "../../../../../../../locales/en/address.json";
+import cyAddressTranslationText from "../../../../../../../locales/cy/address.json";
+import enErrorMessages from "../../../../../../../locales/en/errors.json";
+import cyErrorMessages from "../../../../../../../locales/cy/errors.json";
+
+import app from "../../../app";
+import { getUrl, setLocalesEnabled, testTranslations } from "../../../../utils";
+import { appDevDependencies } from "../../../../../../config/dev-dependencies";
+
+import {
+  CONFIRM_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL,
+  ENTER_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL,
+  POSTCODE_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL,
+  TERRITORY_CHOICE_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_CORRESPONDENCE_ADDRESS_URL,
+} from "../../../../../controller/addressLookUp/url/registration";
+
+import AddressPageType from "../../../../../controller/addressLookUp/PageType";
+import TransactionPersonWithSignificantControl from "../../../../../../domain/entities/TransactionPersonWithSignificantControl";
+import PersonWithSignificantControlBuilder from "../../../../builder/PersonWithSignificantControlBuilder";
+
+describe("Confirm PSC usual residential Address Page", () => {
+  const enTranslationText = { ...enGeneralTranslationText, ...enAddressTranslationText, ...enErrorMessages };
+  const cyTranslationText = { ...cyGeneralTranslationText, ...cyAddressTranslationText, ...cyErrorMessages };
+
+  const URL = getUrl(CONFIRM_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL);
+  const REDIRECT_URL = getUrl(TERRITORY_CHOICE_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_CORRESPONDENCE_ADDRESS_URL);
+  const MANUAL_ENTRY_URL = getUrl(ENTER_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL);
+
+  const pageType = AddressPageType.confirmPersonWithSignificantControlIndividualPersonUsualResidentialAddress;
+
+  let personWithSignificantControl: TransactionPersonWithSignificantControl;
+  beforeEach(() => {
+    setLocalesEnabled(true);
+
+    personWithSignificantControl = new PersonWithSignificantControlBuilder()
+      .isIndividualPerson()
+      .withId(appDevDependencies.personWithSignificantControlGateway.personWithSignificantControlId)
+      .build();
+
+    appDevDependencies.personWithSignificantControlGateway.feedPersonsWithSignificantControl([personWithSignificantControl]);
+
+    appDevDependencies.cacheRepository.feedCache({
+      [appDevDependencies.transactionGateway.transactionId]: {
+        usual_residential_address: {
+          postal_code: "ST6 3LJ",
+          premises: "4",
+          address_line_1: "line 1",
+          address_line_2: "line 2",
+          locality: "stoke-on-trent",
+          region: "region",
+          country: "England"
+        }
+      }
+    });
+  });
+
+  describe("GET Confirm PSC usual residential Address Page", () => {
+    it.each([
+      ["English", "en", enTranslationText],
+      ["Welsh", "cy", cyTranslationText],
+    ])(
+      "should load the confirm PSC usual residential address page with %s text",
+      async (_description: string, lang: string, translationText: Record<string, any>) => {
+        const res = await request(app).get(URL + `?lang=${lang}`);
+
+        expect(res.status).toBe(200);
+        testTranslations(res.text, translationText.address.confirm.personWithSignificantControlUsualResidentialAddress);
+
+        expect(res.text).toContain("4 Line 1");
+        expect(res.text).toContain("Line 2");
+        expect(res.text).toContain("Stoke-On-Trent");
+        expect(res.text).toContain("Region");
+        expect(res.text).toContain("ST6 3LJ");
+
+        expect(res.text).toContain(
+          [
+            personWithSignificantControl?.data?.forename,
+            personWithSignificantControl?.data?.middle_names,
+            personWithSignificantControl?.data?.surname
+          ].join(' ').toUpperCase()
+        );
+      }
+    );
+
+    it.each([
+      [
+        "overseas",
+        getUrl(CONFIRM_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL)
+      ],
+      [
+        "unitedKingdom",
+        getUrl(POSTCODE_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL)
+      ]
+    ])("should have the correct back link for territory for %s", async (territory: string, backLink: string) => {
+      appDevDependencies.cacheRepository.feedCache({
+        [appDevDependencies.transactionGateway.transactionId]: {
+          ura_territory_choice: territory
+        }
+      });
+
+      const res = await request(app).get(URL);
+
+      expect(res.text).toContain(backLink);
+    }
+    );
+
+    it("should redirect to manual address page if address is incomplete", async () => {
+      appDevDependencies.cacheRepository.feedCache({
+        [appDevDependencies.transactionGateway.transactionId]: {
+          usual_residential_address: {
+            postal_code: "ST6 3LJ",
+            premises: "4",
+            address_line_1: "line 1",
+            address_line_2: "line 2",
+            locality: "stoke-on-trent",
+            region: "region",
+            country: null
+          }
+        }
+      });
+
+      const res = await request(app).get(URL);
+
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(`Redirecting to ${MANUAL_ENTRY_URL}`);
+    }
+    );
+  });
+
+  describe("POST Confirm PSC usual residential Address Page", () => {
+    it("should redirect to the next page", async () => {
+      const res = await request(app)
+        .post(URL)
+        .send({
+          pageType,
+          address: `{
+            "postal_code": "ST6 3LJ",
+            "premises": "4",
+            "address_line_1": "DUNCALF STREET",
+            "address_line_2": "",
+            "locality": "STOKE-ON-TRENT",
+            "country": "England"
+          }`
+        });
+
+      expect(res.status).toBe(302);
+      expect(res.text).toContain(`Redirecting to ${REDIRECT_URL}`);
+    });
+
+    it("should show error message if address is not provided", async () => {
+      appDevDependencies.cacheRepository.feedCache({});
+
+      const res = await request(app).post(URL).send({
+        pageType
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.text).toContain("You must provide an address");
+    }
+    );
+
+    it.each([
+      [
+        "en",
+        enTranslationText
+      ],
+      [
+        "cy",
+        cyTranslationText
+      ]
+    ])(
+      "should show validation error message if country is missing with lang %s",
+      async (lang: string, translationText: Record<string, any>) => {
+        setLocalesEnabled(true);
+        const res = await request(app).post(`${URL}?lang=${lang}`).send({
+          pageType,
+          address: `{
+            "postal_code": "ST6 3LJ",
+            "premises": "4",
+            "address_line_1": "DUNCALF STREET",
+            "address_line_2": "",
+            "locality": "STOKE-ON-TRENT",
+            "country": ""
+          }`
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.text).toContain(translationText.errorMessages.address.confirm.countryMissing);
+      }
+    );
+  });
+});
---
+++ src/presentation/test/integration/registration/personWithSignificantControl/address/territory-choice-psc-usual-residential-address.test.ts
@@ -0,0 +1,137 @@
+import request from "supertest";
+
+import enGeneralTranslationText from "../../../../../../../locales/en/translations.json";
+import cyGeneralTranslationText from "../../../../../../../locales/cy/translations.json";
+import enAddressTranslationText from "../../../../../../../locales/en/address.json";
+import cyAddressTranslationText from "../../../../../../../locales/cy/address.json";
+import enErrorsTranslationText from "../../../../../../../locales/en/errors.json";
+import cyErrorsTranslationText from "../../../../../../../locales/cy/errors.json";
+
+import app from "../../../app";
+import { appDevDependencies } from "../../../../../../config/dev-dependencies";
+import { countOccurrences, getUrl, setLocalesEnabled, testTranslations, toEscapedHtml } from "../../../../utils";
+import { APPLICATION_CACHE_KEY } from "../../../../../../config/constants";
+
+import {
+  TERRITORY_CHOICE_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL,
+  POSTCODE_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL,
+  ENTER_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL
+} from "../../../../../controller/addressLookUp/url/registration";
+
+import AddressPageType from "../../../../../controller/addressLookUp/PageType";
+import LimitedPartnershipBuilder from "../../../../builder/LimitedPartnershipBuilder";
+import PersonWithSignificantControlBuilder from "../../../../builder/PersonWithSignificantControlBuilder";
+
+import TransactionPersonWithSignificantControl from "../../../../../../domain/entities/TransactionPersonWithSignificantControl";
+
+describe("PSC usual residential Address Territory Choice", () => {
+  const enTranslationText = { ...enGeneralTranslationText, ...enAddressTranslationText, ...enErrorsTranslationText };
+  const cyTranslationText = { ...cyGeneralTranslationText, ...cyAddressTranslationText, ...cyErrorsTranslationText };
+
+  const URL = getUrl(TERRITORY_CHOICE_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL);
+
+  const pageType = AddressPageType.territoryChoicePersonWithSignificantControlIndividualPersonUsualResidentialAddress;
+
+  let personWithSignificantControl: TransactionPersonWithSignificantControl;
+
+  beforeEach(() => {
+    setLocalesEnabled(true);
+
+    personWithSignificantControl = new PersonWithSignificantControlBuilder()
+      .isIndividualPerson()
+      .withId(appDevDependencies.personWithSignificantControlGateway.personWithSignificantControlId)
+      .build();
+
+    appDevDependencies.personWithSignificantControlGateway.feedPersonsWithSignificantControl([personWithSignificantControl]);
+
+    const limitedPartnership = new LimitedPartnershipBuilder().build();
+    appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
+  });
+
+  describe("GET PSC usual residential address territory choice", () => {
+    it.each([
+      ["English", "en", enTranslationText],
+      ["Welsh", "cy", cyTranslationText]
+    ])(
+      "should load the PSC usual residential address territory choice page with %s text",
+      async (_description: string, lang: string, translationText: Record<string, any>) => {
+        const res = await request(app).get(`${URL}?lang=${lang}`);
+
+        expect(res.status).toBe(200);
+        expect(res.text).toContain(
+          toEscapedHtml(
+            `${translationText.address.territoryChoice.personWithSignificantControlUsualResidentialAddress.title} - ${translationText.serviceRegistration} - GOV.UK`
+          )
+        );
+
+        testTranslations(
+          res.text,
+          translationText.address.territoryChoice.personWithSignificantControlUsualResidentialAddress
+        );
+        testTranslations(res.text, translationText.address.territories);
+
+        expect(res.text).toContain(
+          [
+            personWithSignificantControl?.data?.forename,
+            personWithSignificantControl?.data?.middle_names,
+            personWithSignificantControl?.data?.surname
+          ].join(' ').toUpperCase()
+        );
+      }
+    );
+  });
+
+  describe("POST PSC territory choice", () => {
+    it.each([
+      [
+        "United Kingdom",
+        "unitedKingdom",
+        getUrl(POSTCODE_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL)
+      ],
+      [
+        "Overseas",
+        "overseas",
+        getUrl(ENTER_PERSON_WITH_SIGNIFICANT_CONTROL_INDIVIDUAL_PERSON_USUAL_RESIDENTIAL_ADDRESS_URL)
+      ]
+    ])(
+      "should redirect to postcode lookup page when United Kingdom is selected",
+      async (_description: string, territory: string, expectedRedirectUrl: string) => {
+        const res = await request(app).post(URL).send({
+          pageType,
+          parameter: territory
+        });
+
+        expect(res.status).toBe(302);
+        expect(res.text).toContain(expectedRedirectUrl);
+
+        expect(appDevDependencies.cacheRepository.cache).toEqual({
+          [APPLICATION_CACHE_KEY]: {
+            [appDevDependencies.transactionGateway.transactionId]: {
+              ["ura_territory_choice"]: territory
+            }
+          }
+        });
+      }
+    );
+
+    it("should show an error message when no selection is made for territory choice", async () => {
+      const res = await request(app).post(URL).send({
+        pageType
+      });
+
+      const errorMessages = enTranslationText.errorMessages.address.territoryChoice;
+      const errorMessage = `${errorMessages.noOptionSelectedStart}usual residential address${errorMessages.noOptionSelectedEnd}`;
+
+      expect(res.status).toBe(200);
+      expect(countOccurrences(res.text, errorMessage)).toBe(2);
+      expect(res.text).toContain(
+        [
+          personWithSignificantControl?.data?.forename,
+          personWithSignificantControl?.data?.middle_names,
+          personWithSignificantControl?.data?.surname
+        ].join(' ').toUpperCase()
+      );
+    });
+  });
+});
+
