3c01c932bc4ad499a6a5ead83cc2f71fa4fb8d2e
markpit <11330756+markpit@users.noreply.github.com>
2026-06-08 16:07:19 +0100

    LP-2023 Add happy path for acsp not-eligible tests

--- src/presentation/test/integration/global/not-eligible.test.ts
+++ src/presentation/test/integration/global/not-eligible.test.ts
@@ -4,6 +4,7 @@ import request from "supertest";
 import app from "../app";
 import enTranslationText from "../../../../../locales/en/translations.json";
 import cyTranslationText from "../../../../../locales/cy/translations.json";
+import * as webSecurityNode from "@companieshouse/web-security-node";
 
 import { NOT_ELIGIBLE_URL, RESUME_JOURNEY_POST_TRANSITION_GENERAL_PARTNER_URL, RESUME_JOURNEY_POST_TRANSITION_LIMITED_PARTNER_URL, RESUME_JOURNEY_POST_TRANSITION_PARTNERSHIP_URL, RESUME_JOURNEY_REGISTRATION_OR_TRANSITION_URL } from "../../../controller/global/url";
 import { setLocalesEnabled, testTranslations } from "../../utils";
@@ -71,5 +72,31 @@ describe("Not Eligible Page", () => {
     const res = await request(app).get(url);
     expect(res.status).toBe(403);
     expect(res.text).toContain(enTranslationText.notEligiblePage.title);
+  });
+});
+
+describe("Valid ACSP User", () => {
+  beforeEach(() => {
+    setLocalesEnabled(true);
+    const mocked = jest.mocked(webSecurityNode.acspManageUsersAuthMiddleware);
+    mocked.mockImplementation(() =>
+      (req: Request, res: Response, next: NextFunction) => next()
+    );
+  });
+
+  afterEach(() => {
+    const mocked = jest.mocked(webSecurityNode.acspManageUsersAuthMiddleware);
+    mocked.mockReset();
+  });
+
+  it.each([
+    [NAME_URL, "en"],
+    [GENERAL_PARTNERS_URL, "en"],
+    [NAME_URL, "cy"],
+    [GENERAL_PARTNERS_URL, "cy"]
+  ])("should allow valid ACSP user to access registration route %s - %s", async (url, lang) => {
+    const res = await request(app).get(`${url}?lang=${lang}`);
+    expect(res.status).toBe(200);
+    expect(res.text).not.toContain(enTranslationText.notEligiblePage.title);
   });
 });
