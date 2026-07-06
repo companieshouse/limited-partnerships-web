986d59b5b15278078677ea0c536031c7a1bd1480
lduranteau <lduranteau@companieshouse.gov.uk>
2026-07-06 11:26:36 +0100

    [2056] : populate page with save data

--- src/presentation/test/integration/registration/personWithSignificantControl/add-nature-of-control.test.ts
+++ src/presentation/test/integration/registration/personWithSignificantControl/add-nature-of-control.test.ts
@@ -56,23 +56,73 @@ describe("Which Type of Nature of Control Page", () => {
   describe("Get Add Nature of Control Page", () => {
     it.each([
       [`en - ${NatureOfControlType.INDIVIDUAL}`, "en", getUrl(ADD_NATURE_OF_CONTROL_INDIVIDUAL_URL), enTranslationText, []],
-      [`cy - ${NatureOfControlType.INDIVIDUAL}`, "cy", getUrl(ADD_NATURE_OF_CONTROL_INDIVIDUAL_URL), cyTranslationText, []],
+      [`cy - ${NatureOfControlType.INDIVIDUAL}`, "cy", getUrl(ADD_NATURE_OF_CONTROL_INDIVIDUAL_URL), cyTranslationText, []]
       // [`en - ${NatureOfControlType.FIRM}`, "en", getUrl(ADD_NATURE_OF_CONTROL_FIRM_URL), enTranslationText, ["individual"]],
       // [`cy - ${NatureOfControlType.FIRM}`, "cy", getUrl(ADD_NATURE_OF_CONTROL_FIRM_URL), cyTranslationText, ["individual"]],
       // [`en - ${NatureOfControlType.TRUST}`, "en", getUrl(ADD_NATURE_OF_CONTROL_TRUST_URL), enTranslationText, ["individual"]],
       // [`cy - ${NatureOfControlType.TRUST}`, "cy", getUrl(ADD_NATURE_OF_CONTROL_TRUST_URL), cyTranslationText, ["individual"]]
-    ])("should load the add nature of control page - %s", async (_description: string, lang: string, url: string, translationText: Record<string, any>, excludedTexts: string[]) => {
-      const res = await request(app).get(`${url}?lang=${lang}`);
+    ])(
+      "should load the add nature of control page - %s",
+      async (_description: string, lang: string, url: string, translationText: Record<string, any>, excludedTexts: string[]) => {
+        const res = await request(app).get(`${url}?lang=${lang}`);
 
-      expect(res.status).toBe(200);
+        expect(res.status).toBe(200);
 
-      testTranslations(res.text, translationText.personWithSignificantControl.addNatureOfControlPage, excludedTexts);
+        testTranslations(res.text, translationText.personWithSignificantControl.addNatureOfControlPage, excludedTexts);
 
-      expect(res.text).toContain(individualPerson.data?.legal_entity_name?.toUpperCase());
+        expect(res.text).toContain(individualPerson.data?.legal_entity_name?.toUpperCase());
 
-      const backUrl = getUrl(WHICH_TYPE_OF_NATURE_OF_CONTROL_INDIVIDUAL_PERSON_URL);
-      expect(res.text).toContain(backUrl);
-    });
+        const backUrl = getUrl(WHICH_TYPE_OF_NATURE_OF_CONTROL_INDIVIDUAL_PERSON_URL);
+        expect(res.text).toContain(backUrl);
+      }
+    );
+
+    it.each([
+      [`en - ${NatureOfControlType.INDIVIDUAL}`, "en", getUrl(ADD_NATURE_OF_CONTROL_INDIVIDUAL_URL), enTranslationText, []],
+      [`cy - ${NatureOfControlType.INDIVIDUAL}`, "cy", getUrl(ADD_NATURE_OF_CONTROL_INDIVIDUAL_URL), cyTranslationText, []]
+    ])(
+      "should load the add nature of control page with data from api - %s",
+      async (_description: string, lang: string, url: string, translationText: Record<string, any>, excludedTexts: string[]) => {
+        const personWithNatureOfControl = new PersonWithSignificantControlBuilder()
+          .isIndividualPerson()
+          .withId(appDevDependencies.personWithSignificantControlGateway.personWithSignificantControlId)
+          .withNaturesOfControl([
+            {
+              type: NatureOfControlType.INDIVIDUAL,
+              share_of_assets_25_to_50: true,
+              voting_rights_25_to_50: true,
+              right_to_appointment_and_remove: true
+            },
+            {
+              type: NatureOfControlType.FIRM,
+              share_of_assets_50_to_75: true,
+              voting_rights_50_to_75: true,
+              right_to_appointment_and_remove: true
+            }
+          ])
+          .build();
+
+        appDevDependencies.personWithSignificantControlGateway.feedPersonsWithSignificantControl([personWithNatureOfControl]);
+
+        const res = await request(app).get(`${url}?lang=${lang}`);
+
+        expect(res.status).toBe(200);
+
+        testTranslations(res.text, translationText.personWithSignificantControl.addNatureOfControlPage, excludedTexts);
+
+        expect(res.text).toContain(individualPerson.data?.legal_entity_name?.toUpperCase());
+
+        expect(res.text).toContain('value="share_of_assets_25_to_50" checked');
+        expect(res.text).not.toContain('value="share_of_assets_50_to_75" checked');
+        expect(res.text).toContain('value="voting_rights_25_to_50" checked');
+        expect(res.text).not.toContain('value="voting_rights_50_to_75" checked');
+        expect(res.text).toContain('name="right_to_appointment_and_remove" type="checkbox" value="true" checked');
+        expect(res.text).not.toContain('name="significant_influence_control" type="checkbox" value="true" checked');
+
+        const backUrl = getUrl(WHICH_TYPE_OF_NATURE_OF_CONTROL_INDIVIDUAL_PERSON_URL);
+        expect(res.text).toContain(backUrl);
+      }
+    );
   });
 
   describe("Post Add Nature of Control Page", () => {
