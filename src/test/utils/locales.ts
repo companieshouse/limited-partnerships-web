954e88d70102c6092cc08710537a9f9ce8d57d06
markpit <11330756+markpit@users.noreply.github.com>
2026-07-27 16:52:05 +0100

    LP-2191 Move nationalities into own translations file

--- src/test/utils/locales.ts
+++ src/test/utils/locales.ts
@@ -1,12 +1,14 @@
 import enAddress from "../../../locales/en/address.json";
 import enCountries from "../../../locales/en/countries.json";
 import enErrors from "../../../locales/en/errors.json";
+import enNationalities from "../../../locales/en/nationalities.json";
 import enPsc from "../../../locales/en/personWithSignificantControl.json";
 import enSicCodes from "../../../locales/en/sicCodes.json";
 import enRootTranslations from "../../../locales/en/translations.json";
 import cyAddress from "../../../locales/cy/address.json";
 import cyCountries from "../../../locales/cy/countries.json";
 import cyErrors from "../../../locales/cy/errors.json";
+import cyNationalities from "../../../locales/cy/nationalities.json";
 import cyPsc from "../../../locales/cy/personWithSignificantControl.json";
 import cySicCodes from "../../../locales/cy/sicCodes.json";
 import cyRootTranslations from "../../../locales/cy/translations.json";
@@ -16,6 +18,7 @@ export const enTranslationText = {
   ...enAddress,
   ...enCountries,
   ...enErrors,
+  ...enNationalities,
   ...enPsc,
   ...enSicCodes
 };
@@ -25,6 +28,7 @@ export const cyTranslationText = {
   ...cyAddress,
   ...cyCountries,
   ...cyErrors,
+  ...cyNationalities,
   ...cyPsc,
   ...cySicCodes
 };
--- src/utils/nationalities.ts
+++ src/utils/nationalities.ts
@@ -1,16 +1,17 @@
-import { enTranslation } from "../config/localesTranslations";
+import enNationalitiesText from "../../locales/en/nationalities.json";
 
 export const setNationalitiesDropdown = (i18n: Record<string, any>, nationalityField: string | undefined, selectPrompt: string) => {
   const nationalities: { value: string; text: string; selected: boolean }[] = [];
   const ukNationalities: { value: string; text: string; selected: boolean }[] = [];
+  const enNationalities = enNationalitiesText.nationalities as Record<string, string>;
 
   const ukNationalitiesKeys = ["british", "english", "northernIrish", "scottish", "welsh"];
 
   for (const nationality in i18n.nationalities) {
     if (ukNationalitiesKeys.includes(nationality)) {
-      ukNationalities.push({ value: enTranslation.nationalities[nationality], text: i18n.nationalities[nationality], selected: nationalityField?.toLowerCase() === enTranslation.nationalities[nationality]?.toLowerCase() });
+      ukNationalities.push({ value: enNationalities[nationality], text: i18n.nationalities[nationality], selected: nationalityField?.toLowerCase() === enNationalities[nationality]?.toLowerCase() });
     } else {
-      nationalities.push({ value: enTranslation.nationalities[nationality], text: i18n.nationalities[nationality], selected: nationalityField?.toLowerCase() === enTranslation.nationalities[nationality]?.toLowerCase() });
+      nationalities.push({ value: enNationalities[nationality], text: i18n.nationalities[nationality], selected: nationalityField?.toLowerCase() === enNationalities[nationality]?.toLowerCase() });
     }
   }
 
