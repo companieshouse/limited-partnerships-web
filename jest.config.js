7ce2d7faf3eb42a2f989f292a10aa84b406940eb
markpit <11330756+markpit@users.noreply.github.com>
2026-07-28 17:20:14 +0100

    LP-2191 switch back to ts-jest

--- jest.config.js
+++ jest.config.js
@@ -3,21 +3,7 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
-    '^.+\\.(ts|js)$': ['@swc/jest', {
-      jsc: {
-        parser: {
-          syntax: 'typescript',
-          decorators: true,
-        },
-        transform: {
-          legacyDecorator: true,
-          decoratorMetadata: true,
-        },
-      },
-      module: {
-        type: 'commonjs',
-      },
-    }],
+    '^.+\\.(ts|js)$': require.resolve('ts-jest'),
   },
   transformIgnorePatterns: [
     '/node_modules/(?!uuid/)',
