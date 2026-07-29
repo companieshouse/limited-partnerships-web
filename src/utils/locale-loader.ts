50b2ecbb20b1258462a6d1e072a0ca8c7af2784a
markpit <11330756+markpit@users.noreply.github.com>
2026-07-29 12:24:32 +0100

    LP-2191 add locale param to locales loader

--- src/utils/locale-loader.ts
+++ src/utils/locale-loader.ts
@@ -1,10 +1,12 @@
 import { readFileSync } from "fs";
 import path from "path";
 
+// service fails to start in CI-Dev if using relative path to locales folder,
+// so this utility uses an absolute path to the locales folder to load the json files
 const LOCALES_PATH = path.resolve(process.cwd(), "locales");
 
-export const loadLocaleJson = <T>(fileName: string): T => {
-  const filePath = path.join(LOCALES_PATH, "en", fileName);
+export const loadLocaleJson = <T>(fileName: string, locale: string = "en"): T => {
+  const filePath = path.join(LOCALES_PATH, locale, fileName);
   const fileContents = readFileSync(filePath, "utf-8");
   return JSON.parse(fileContents) as T;
 };
