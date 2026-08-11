4db61a6324ba2b11397172020dd255c18e13c6f5
markpit <11330756+markpit@users.noreply.github.com>
2026-08-11 09:27:42 +0100

    LP-2194 make yes no test configs a function

--- src/presentation/test/integration/postTransition/update-address-yes-no.test.ts
+++ src/presentation/test/integration/postTransition/update-address-yes-no.test.ts
@@ -32,100 +32,7 @@ import TransactionGeneralPartner from "../../../../domain/entities/TransactionGe
 import TransactionLimitedPartner from "../../../../domain/entities/TransactionLimitedPartner";
 import { enTranslationText, cyTranslationText } from "../../../../test/utils/locales";
 
-const testConfigs = [
-  {
-    description: "Update Principal Office Address Yes No Page",
-    urlConstant: UPDATE_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_YES_NO_URL,
-    redirectYesConstant: ENTER_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
-    redirectNoConstant: WHEN_DID_GENERAL_PARTNER_LEGAL_ENTITY_DETAILS_CHANGE_URL,
-    partnerKind: PartnerKind.UPDATE_GENERAL_PARTNER_LEGAL_ENTITY,
-    getPartnerId: () => appDevDependencies.generalPartnerGateway.generalPartnerId,
-    buildPartner: (id: string) => new GeneralPartnerBuilder().withId(id).isLegalEntity().build(),
-    setupGateway: (p: TransactionGeneralPartner) => {
-      appDevDependencies.generalPartnerGateway.feedGeneralPartners([p]);
-      appDevDependencies.generalPartnerGateway.feedErrors();
-    },
-    getPartnerName: (gp: any) => `${gp.data?.legal_entity_name?.toUpperCase()}`,
-    getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.principalOfficeAddress,
-    getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateGeneralPartnerLegalEntity,
-    fieldName: "update_principal_office_address_required",
-    pageType: PostTransitionPageType.updateGeneralPartnerPrincipalOfficeAddressYesNo
-  },
-  {
-    description: "Update Usual Residential Address Yes No Page",
-    urlConstant: UPDATE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_YES_NO_URL,
-    redirectYesConstant: TERRITORY_CHOICE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
-    redirectNoConstant: UPDATE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_YES_NO_URL,
-    partnerKind: PartnerKind.UPDATE_GENERAL_PARTNER_PERSON,
-    getPartnerId: () => appDevDependencies.generalPartnerGateway.generalPartnerId,
-    buildPartner: (id: string) => new GeneralPartnerBuilder().withId(id).isPerson().build(),
-    setupGateway: (p: TransactionGeneralPartner) => {
-      appDevDependencies.generalPartnerGateway.feedGeneralPartners([p]);
-      appDevDependencies.generalPartnerGateway.feedErrors();
-    },
-    getPartnerName: (gp: any) => `${gp.data?.forename?.toUpperCase()} ${gp.data?.surname?.toUpperCase()}`,
-    getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.usualResidentialAddress,
-    getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateGeneralPartnerPerson,
-    fieldName: "update_usual_residential_address_required",
-    pageType: PostTransitionPageType.updateGeneralPartnerUsualResidentialAddressYesNo
-  },
-  {
-    description: "Update Correspondence Address Yes No Page",
-    urlConstant: UPDATE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_YES_NO_URL,
-    redirectYesConstant: ENTER_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
-    redirectNoConstant: "/",
-    partnerKind: PartnerKind.UPDATE_GENERAL_PARTNER_PERSON,
-    getPartnerId: () => appDevDependencies.generalPartnerGateway.generalPartnerId,
-    buildPartner: (id: string) => new GeneralPartnerBuilder().withId(id).isPerson().build(),
-    setupGateway: (p: TransactionGeneralPartner) => {
-      appDevDependencies.generalPartnerGateway.feedGeneralPartners([p]);
-      appDevDependencies.generalPartnerGateway.feedErrors();
-    },
-    getPartnerName: (gp: any) => `${gp.data?.forename?.toUpperCase()} ${gp.data?.surname?.toUpperCase()}`,
-    getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.correspondenceAddress,
-    getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateGeneralPartnerPerson,
-    fieldName: "update_service_address_required",
-    pageType: PostTransitionPageType.updateCorrespondenceAddressYesNo
-  },
-  {
-    description: "Update Limited Partner Usual Residential Address Yes No Page",
-    urlConstant: UPDATE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_YES_NO_URL,
-    redirectYesConstant: TERRITORY_CHOICE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
-    redirectNoConstant: WHEN_DID_LIMITED_PARTNER_PERSON_DETAILS_CHANGE_URL,
-    partnerKind: PartnerKind.UPDATE_LIMITED_PARTNER_PERSON,
-    getPartnerId: () => appDevDependencies.limitedPartnerGateway.limitedPartnerId,
-    buildPartner: (id: string) => new LimitedPartnerBuilder().withId(id).isPerson().build(),
-    setupGateway: (p: TransactionLimitedPartner) => {
-      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([p]);
-      appDevDependencies.limitedPartnerGateway.feedErrors();
-    },
-    getPartnerName: (gp: any) => `${gp.data?.forename?.toUpperCase()} ${gp.data?.surname?.toUpperCase()}`,
-    getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.limitedPartnerUsualResidentialAddress,
-    getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateLimitedPartnerPerson,
-    fieldName: "update_usual_residential_address_required",
-    pageType: PostTransitionPageType.updateLimitedPartnerUsualResidentialAddressYesNo
-  },
-  {
-    description: "Update Limited Partner Principal Office Address Yes No Page",
-    urlConstant: UPDATE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_YES_NO_URL,
-    redirectYesConstant: ENTER_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
-    redirectNoConstant: WHEN_DID_LIMITED_PARTNER_LEGAL_ENTITY_DETAILS_CHANGE_URL,
-    partnerKind: PartnerKind.UPDATE_LIMITED_PARTNER_LEGAL_ENTITY,
-    getPartnerId: () => appDevDependencies.limitedPartnerGateway.limitedPartnerId,
-    buildPartner: (id: string) => new LimitedPartnerBuilder().withId(id).isLegalEntity().build(),
-    setupGateway: (p: TransactionLimitedPartner) => {
-      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([p]);
-      appDevDependencies.limitedPartnerGateway.feedErrors();
-    },
-    getPartnerName: (gp: any) => `${gp.data?.forename?.toUpperCase()} ${gp.data?.surname?.toUpperCase()}`,
-    getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.limitedPartnerPrincipalOfficeAddress,
-    getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateLimitedPartnerLegalEntity,
-    fieldName: "update_principal_office_address_required",
-    pageType: PostTransitionPageType.updateLimitedPartnerPrincipalOfficeAddressYesNo
-  }
-];
-
-describe.each(testConfigs)(
+describe.each(testConfigs())(
   "$description",
   ({ urlConstant, redirectYesConstant, redirectNoConstant, partnerKind, getPartnerId, buildPartner, setupGateway, getPartnerName, getAddressTranslations, getServiceNameTranslation, fieldName, pageType }) => {
     const URL = getUrl(urlConstant);
@@ -204,3 +111,98 @@ describe.each(testConfigs)(
     });
   }
 );
+
+function testConfigs() {
+  return [
+    {
+      description: "Update Principal Office Address Yes No Page",
+      urlConstant: UPDATE_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_YES_NO_URL,
+      redirectYesConstant: ENTER_GENERAL_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+      redirectNoConstant: WHEN_DID_GENERAL_PARTNER_LEGAL_ENTITY_DETAILS_CHANGE_URL,
+      partnerKind: PartnerKind.UPDATE_GENERAL_PARTNER_LEGAL_ENTITY,
+      getPartnerId: () => appDevDependencies.generalPartnerGateway.generalPartnerId,
+      buildPartner: (id: string) => new GeneralPartnerBuilder().withId(id).isLegalEntity().build(),
+      setupGateway: (p: TransactionGeneralPartner) => {
+        appDevDependencies.generalPartnerGateway.feedGeneralPartners([p]);
+        appDevDependencies.generalPartnerGateway.feedErrors();
+      },
+      getPartnerName: (gp: any) => `${gp.data?.legal_entity_name?.toUpperCase()}`,
+      getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.principalOfficeAddress,
+      getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateGeneralPartnerLegalEntity,
+      fieldName: "update_principal_office_address_required",
+      pageType: PostTransitionPageType.updateGeneralPartnerPrincipalOfficeAddressYesNo
+    },
+    {
+      description: "Update Usual Residential Address Yes No Page",
+      urlConstant: UPDATE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_YES_NO_URL,
+      redirectYesConstant: TERRITORY_CHOICE_GENERAL_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
+      redirectNoConstant: UPDATE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_YES_NO_URL,
+      partnerKind: PartnerKind.UPDATE_GENERAL_PARTNER_PERSON,
+      getPartnerId: () => appDevDependencies.generalPartnerGateway.generalPartnerId,
+      buildPartner: (id: string) => new GeneralPartnerBuilder().withId(id).isPerson().build(),
+      setupGateway: (p: TransactionGeneralPartner) => {
+        appDevDependencies.generalPartnerGateway.feedGeneralPartners([p]);
+        appDevDependencies.generalPartnerGateway.feedErrors();
+      },
+      getPartnerName: (gp: any) => `${gp.data?.forename?.toUpperCase()} ${gp.data?.surname?.toUpperCase()}`,
+      getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.usualResidentialAddress,
+      getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateGeneralPartnerPerson,
+      fieldName: "update_usual_residential_address_required",
+      pageType: PostTransitionPageType.updateGeneralPartnerUsualResidentialAddressYesNo
+    },
+    {
+      description: "Update Correspondence Address Yes No Page",
+      urlConstant: UPDATE_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_YES_NO_URL,
+      redirectYesConstant: ENTER_GENERAL_PARTNER_CORRESPONDENCE_ADDRESS_URL,
+      redirectNoConstant: "/",
+      partnerKind: PartnerKind.UPDATE_GENERAL_PARTNER_PERSON,
+      getPartnerId: () => appDevDependencies.generalPartnerGateway.generalPartnerId,
+      buildPartner: (id: string) => new GeneralPartnerBuilder().withId(id).isPerson().build(),
+      setupGateway: (p: TransactionGeneralPartner) => {
+        appDevDependencies.generalPartnerGateway.feedGeneralPartners([p]);
+        appDevDependencies.generalPartnerGateway.feedErrors();
+      },
+      getPartnerName: (gp: any) => `${gp.data?.forename?.toUpperCase()} ${gp.data?.surname?.toUpperCase()}`,
+      getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.correspondenceAddress,
+      getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateGeneralPartnerPerson,
+      fieldName: "update_service_address_required",
+      pageType: PostTransitionPageType.updateCorrespondenceAddressYesNo
+    },
+    {
+      description: "Update Limited Partner Usual Residential Address Yes No Page",
+      urlConstant: UPDATE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_YES_NO_URL,
+      redirectYesConstant: TERRITORY_CHOICE_LIMITED_PARTNER_USUAL_RESIDENTIAL_ADDRESS_URL,
+      redirectNoConstant: WHEN_DID_LIMITED_PARTNER_PERSON_DETAILS_CHANGE_URL,
+      partnerKind: PartnerKind.UPDATE_LIMITED_PARTNER_PERSON,
+      getPartnerId: () => appDevDependencies.limitedPartnerGateway.limitedPartnerId,
+      buildPartner: (id: string) => new LimitedPartnerBuilder().withId(id).isPerson().build(),
+      setupGateway: (p: TransactionLimitedPartner) => {
+        appDevDependencies.limitedPartnerGateway.feedLimitedPartners([p]);
+        appDevDependencies.limitedPartnerGateway.feedErrors();
+      },
+      getPartnerName: (gp: any) => `${gp.data?.forename?.toUpperCase()} ${gp.data?.surname?.toUpperCase()}`,
+      getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.limitedPartnerUsualResidentialAddress,
+      getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateLimitedPartnerPerson,
+      fieldName: "update_usual_residential_address_required",
+      pageType: PostTransitionPageType.updateLimitedPartnerUsualResidentialAddressYesNo
+    },
+    {
+      description: "Update Limited Partner Principal Office Address Yes No Page",
+      urlConstant: UPDATE_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_YES_NO_URL,
+      redirectYesConstant: ENTER_LIMITED_PARTNER_PRINCIPAL_OFFICE_ADDRESS_URL,
+      redirectNoConstant: WHEN_DID_LIMITED_PARTNER_LEGAL_ENTITY_DETAILS_CHANGE_URL,
+      partnerKind: PartnerKind.UPDATE_LIMITED_PARTNER_LEGAL_ENTITY,
+      getPartnerId: () => appDevDependencies.limitedPartnerGateway.limitedPartnerId,
+      buildPartner: (id: string) => new LimitedPartnerBuilder().withId(id).isLegalEntity().build(),
+      setupGateway: (p: TransactionLimitedPartner) => {
+        appDevDependencies.limitedPartnerGateway.feedLimitedPartners([p]);
+        appDevDependencies.limitedPartnerGateway.feedErrors();
+      },
+      getPartnerName: (gp: any) => `${gp.data?.forename?.toUpperCase()} ${gp.data?.surname?.toUpperCase()}`,
+      getAddressTranslations: (translationText: typeof enTranslationText | typeof cyTranslationText) => translationText.address.update.limitedPartnerPrincipalOfficeAddress,
+      getServiceNameTranslation: (translationText: typeof enTranslationText) => translationText.serviceName.updateLimitedPartnerLegalEntity,
+      fieldName: "update_principal_office_address_required",
+      pageType: PostTransitionPageType.updateLimitedPartnerPrincipalOfficeAddressYesNo
+    }
+  ];
+}
