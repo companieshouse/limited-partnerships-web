adb302717dbe4139850f851d9ed03d8186688bc0
ollierudge-CH <orudge@companieshouse.gov.uk>
2026-08-17 13:02:46 +0100

    Added error messages and test cases for 3 scenarios needed

--- src/presentation/test/integration/postTransition/limitedPartnership/redesignate-to-pflp.test.ts
+++ src/presentation/test/integration/postTransition/limitedPartnership/redesignate-to-pflp.test.ts
@@ -8,8 +8,6 @@ import CompanyProfileBuilder from "../../../builder/CompanyProfileBuilder";
 import PostTransitionPageType from "../../../../controller/postTransition/pageType";
 import { Jurisdiction, PartnershipType } from "@companieshouse/api-sdk-node/dist/services/limited-partnerships";
 import { TRANSACTION_DESCRIPTION_DESIGNATE_AS_PRIVATE_FUND_PARTNERSHIP, YOUR_COMPANY_URL } from "../../../../../config/index";
-import LimitedPartnershipBuilder from "../../../builder/LimitedPartnershipBuilder";
-import { ApiErrors } from "../../../../../domain/entities/UIErrors";
 import { customerFeedbackUrlMap } from "../../../../../middlewares/customer-feedback.middleware";
 import { enTranslationText, cyTranslationText } from "../../../../../test/utils/locales";
 describe("Redesignate to pflp page", () => {
@@ -74,35 +72,56 @@ describe("Redesignate to pflp page", () => {
       expect(appDevDependencies.transactionGateway.transactions[0].description).toEqual(TRANSACTION_DESCRIPTION_DESIGNATE_AS_PRIVATE_FUND_PARTNERSHIP);
     });
 
-    it("should return a validation error if api validation error occurs creating LimitedPartnership", async () => {
-      const limitedPartnership = new LimitedPartnershipBuilder().build();
-      appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
-      const apiErrors: ApiErrors = {
-        errors: { something: "Something is invalid" }
-      };
-      appDevDependencies.limitedPartnershipGateway.feedErrors(apiErrors);
-
-      const res = await request(app)
-        .post(URL)
-        .send({
-          pageType: PostTransitionPageType.enterPrincipalPlaceOfBusinessAddress,
-          ...limitedPartnership.data?.principal_place_of_business_address
-        });
-
-      expect(res.status).toBe(200);
-      expect(res.text).toContain(enTranslationText.govUk.error.title);
-      expect(res.text).toContain(companyProfile.data.companyName.toUpperCase());
-      expect(res.text).toContain("Something is invalid");
-    });
-
     it("should throw error when payment redirect url is missing", async () => {
       appDevDependencies.paymentGateway.feedPaymentWithEmptyJourney();
       const res = await request(app).post(URL).send({
-        pageType: PostTransitionPageType.redesignateToPflp
+        pageType: PostTransitionPageType.redesignateToPflp,
+        redesignate_to_pflp_apply: true,
+        redesignate_to_pflp_confirm: true,
+        partnership_type: PartnershipType.LP
       });
 
       expect(res.status).toBe(500);
       expect(res.text).not.toContain(`Redirecting to ${PAYMENT_LINK_JOURNEY}`);
+    });
+
+    it("should render the page with errors if the user does not select both checkboxes", async () => {
+      const res = await request(app).post(URL).send({
+        pageType: PostTransitionPageType.redesignateToPflp,
+        redesignate_to_pflp_apply: false,
+        redesignate_to_pflp_confirm: false,
+        partnership_type: PartnershipType.LP
+      });
+
+      const errorMessage = enTranslationText.errorMessages.redesignateToPflpPage.bothRequired;
+      expect(res.status).toBe(200);
+      expect(countOccurrences(res.text, errorMessage)).toBe(2);
+    });
+
+    it("should render the page with errors if the user does not select the apply checkbox", async () => {
+      const res = await request(app).post(URL).send({
+        pageType: PostTransitionPageType.redesignateToPflp,
+        redesignate_to_pflp_apply: false,
+        redesignate_to_pflp_confirm: true,
+        partnership_type: PartnershipType.LP
+      });
+
+      const errorMessage = enTranslationText.errorMessages.redesignateToPflpPage.applyRequired;
+      expect(res.status).toBe(200);
+      expect(countOccurrences(res.text, errorMessage)).toBe(2);
+    });
+
+    it("should render the page with errors if the user does not select the confirm checkbox", async () => {
+      const res = await request(app).post(URL).send({
+        pageType: PostTransitionPageType.redesignateToPflp,
+        redesignate_to_pflp_apply: true,
+        redesignate_to_pflp_confirm: false,
+        partnership_type: PartnershipType.LP
+      });
+
+      const errorMessage = enTranslationText.errorMessages.redesignateToPflpPage.confirmationRequired;
+      expect(res.status).toBe(200);
+      expect(countOccurrences(res.text, errorMessage)).toBe(2);
     });
   });
 
