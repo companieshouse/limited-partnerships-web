2530cb147b01107e8e7d3a1ccd485cf9a94420ce
lduranteau <lduranteau@companieshouse.gov.uk>
2026-08-21 10:26:19 +0100

    [lp-2194] : merge general-partner-choice tests

--- src/presentation/test/integration/postTransition/generalPartner/general-partner-choice.test.ts
+++ src/presentation/test/integration/postTransition/generalPartner/general-partner-choice.test.ts
@@ -1,74 +1,22 @@
-import request from "supertest";
-
-import app from "../../app";
 import {
   ADD_GENERAL_PARTNER_LEGAL_ENTITY_URL,
   ADD_GENERAL_PARTNER_PERSON_URL,
   GENERAL_PARTNER_CHOICE_URL
 } from "../../../../controller/postTransition/url";
+
 import PostTransitionPageType from "../../../../controller/postTransition/pageType";
-import { appDevDependencies } from "../../../../../config/dev-dependencies";
-import { countOccurrences, getUrl, setLocalesEnabled, testTranslations } from "../../../utils";
-import CompanyProfileBuilder from "../../../builder/CompanyProfileBuilder";
-import { enTranslationText, cyTranslationText } from "../../../../../test/utils/locales";
-import { YOUR_COMPANY_OFFICERS_URL } from "../../../../../config";
-describe("General Partner Choice Page", () => {
-  const URL = getUrl(GENERAL_PARTNER_CHOICE_URL);
-  const BACK_LINK = getUrl(YOUR_COMPANY_OFFICERS_URL);
 
-  let companyProfile;
+import { runGeneralPartnerChoiceTests } from "../../shared/generalPartnerChoice";
 
-  beforeEach(() => {
-    setLocalesEnabled(false);
+it("should run general partner choice tests for post-transition journey", () => {
+  expect(ADD_GENERAL_PARTNER_PERSON_URL).toContain("update");
+});
 
-    companyProfile = new CompanyProfileBuilder().build();
-    appDevDependencies.companyGateway.feedCompanyProfile(companyProfile.data);
-  });
-
-  it.each([
-    ["en", enTranslationText],
-    ["cy", cyTranslationText]
-  ])("should load the general partner choice page with %s text", async (lang, translation) => {
-    setLocalesEnabled(true);
-    const res = await request(app).get(URL + `?lang=${lang}`);
-
-    expect(res.status).toBe(200);
-    expect(res.text).toContain(
-      `${translation.partner.generalPartnerChoicePage.title} - ${translation.serviceName.addGeneralPartner} - GOV.UK`
-    );
-    expect(res.text).toContain(
-      `${companyProfile.data.companyName.toUpperCase()} (${companyProfile.data.companyNumber.toUpperCase()})`
-    );
-    testTranslations(res.text, translation.partner.generalPartnerChoicePage, ["hint"]);
-    expect(countOccurrences(res.text, translation.serviceName.addGeneralPartner)).toBe(2);
-    expect(res.text).toContain(BACK_LINK);
-  });
-
-  it.each([
-    ["person", getUrl(ADD_GENERAL_PARTNER_PERSON_URL)],
-    ["legal entity", getUrl(ADD_GENERAL_PARTNER_LEGAL_ENTITY_URL)]
-  ])("should redirect to the General Partner %s page when %s is selected", async (partnerType, expectedUrl) => {
-    const res = await request(app).post(URL).send({
-      pageType: PostTransitionPageType.generalPartnerType,
-      parameter: partnerType
-    });
-
-    expect(res.status).toBe(302);
-    expect(res.text).toContain(expectedUrl);
-  });
-
-  it.each([
-    ["en", enTranslationText],
-    ["cy", cyTranslationText]
-  ])("%s: should trigger GDS validation error when no option is selected", async (lang, errors) => {
-    setLocalesEnabled(true);
-    const res = await request(app).post(URL + `?lang=${lang}`).send({
-      pageType: PostTransitionPageType.generalPartnerType
-    });
-
-    const errorMessage = errors.errorMessages.choosePartnerType.generalPartner;
-
-    expect(res.status).toBe(200);
-    expect(countOccurrences(res.text, errorMessage)).toBe(2);
-  });
+runGeneralPartnerChoiceTests({
+  url: GENERAL_PARTNER_CHOICE_URL,
+  pageType: PostTransitionPageType.generalPartnerType,
+  redirectUrlPerson: ADD_GENERAL_PARTNER_PERSON_URL,
+  redirectUrlLegalEntity: ADD_GENERAL_PARTNER_LEGAL_ENTITY_URL,
+  translateExclude: ["hint"],
+  serviceTitleTranslationKey: { serviceName: "addGeneralPartner" }
 });
