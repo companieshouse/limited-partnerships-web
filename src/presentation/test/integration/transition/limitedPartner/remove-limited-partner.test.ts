4051e931a771bfe1b931ff6f366867b7d4e99913
lduranteau <lduranteau@companieshouse.gov.uk>
2026-08-27 09:33:25 +0100

    [lp-2194] : merge remove-limited-partner tests

--- src/presentation/test/integration/transition/limitedPartner/remove-limited-partner.test.ts
+++ src/presentation/test/integration/transition/limitedPartner/remove-limited-partner.test.ts
@@ -1,132 +1,21 @@
-import request from "supertest";
-
-import app from "../../app";
-import { appDevDependencies } from "../../../../../config/dev-dependencies";
-
-import LimitedPartnerBuilder from "../../../builder/LimitedPartnerBuilder";
-import LimitedPartnershipBuilder from "../../../builder/LimitedPartnershipBuilder";
-import { getUrl, setLocalesEnabled, testTranslations } from "../../../utils";
 import { REMOVE_LIMITED_PARTNER_URL, REVIEW_LIMITED_PARTNERS_URL } from "../../../../controller/transition/url";
-import RegistrationPageType from "../../../../controller/transition/PageType";
-import { enTranslationText, cyTranslationText } from "../../../../../test/utils/locales";
-describe("Remove Limited Partner Page", () => {
-  const URL = getUrl(REMOVE_LIMITED_PARTNER_URL);
 
-  beforeEach(() => {
-    setLocalesEnabled(false);
+import TransitionPageType from "../../../../controller/transition/PageType";
 
-    appDevDependencies.limitedPartnerGateway.feedLimitedPartners([]);
-  });
+import { SERVICE_NAME_KEY_TRANSITION } from "../../../../../config/constants";
 
-  describe("Get Remove Limited Partners Page", () => {
-    it("should load the remove limited partners page with English text", async () => {
-      setLocalesEnabled(true);
+import { runRemoveLimitedPartnerTests } from "../../shared/limitedPartner/removeLimitedPartner";
 
-      const limitedPartnerPerson = new LimitedPartnerBuilder()
-        .isPerson()
-        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
-        .build();
+it("should run remove limited partner tests for transition journey", () => {
+  expect(REMOVE_LIMITED_PARTNER_URL).toContain("transition");
+});
 
-      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartnerPerson]);
-
-      const res = await request(app).get(URL + "?lang=en");
-
-      expect(res.status).toBe(200);
-
-      expect(res.text).toContain(
-        `${enTranslationText.partner.removePartnerPage.title} - ${enTranslationText.serviceTransition} - GOV.UK`
-      );
-
-      testTranslations(res.text, enTranslationText.partner.removePartnerPage);
-
-      expect(res.text).toContain(`${limitedPartnerPerson?.data?.forename} ${limitedPartnerPerson?.data?.surname}`);
-    });
-
-    it("should contain the partnership name above the page title", async () => {
-      const limitedPartnership = new LimitedPartnershipBuilder().build();
-
-      appDevDependencies.limitedPartnershipGateway.feedLimitedPartnerships([limitedPartnership]);
-
-      const limitedPartnerPerson = new LimitedPartnerBuilder()
-        .isPerson()
-        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
-        .build();
-
-      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartnerPerson]);
-
-      const res = await request(app).get(URL);
-
-      expect(res.status).toBe(200);
-      expect(res.text).toContain(
-        `${limitedPartnership?.data?.partnership_name?.toUpperCase()} (${limitedPartnership?.data?.partnership_number?.toUpperCase()})`
-      );
-    });
-
-    it("should load the remove limited partners page with Welsh text", async () => {
-      setLocalesEnabled(true);
-
-      const limitedPartnerLegalEntity = new LimitedPartnerBuilder()
-        .isLegalEntity()
-        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
-        .build();
-
-      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartnerLegalEntity]);
-
-      const res = await request(app).get(URL + "?lang=cy");
-
-      expect(res.status).toBe(200);
-
-      expect(res.text).toContain(
-        `${cyTranslationText.partner.removePartnerPage.title} - ${cyTranslationText.serviceTransition} - GOV.UK`
-      );
-
-      testTranslations(res.text, cyTranslationText.partner.removePartnerPage);
-
-      expect(res.text).toContain(`${limitedPartnerLegalEntity?.data?.legal_entity_name}`);
-    });
-  });
-
-  describe("Post Remove Limited Partners Page", () => {
-    it("should redirect to the review limited partners page - lp removed", async () => {
-      setLocalesEnabled(true);
-
-      const limitedPartnerPerson = new LimitedPartnerBuilder()
-        .isPerson()
-        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
-        .build();
-
-      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartnerPerson]);
-
-      const res = await request(app).post(URL).send({
-        pageType: RegistrationPageType.removeLimitedPartner,
-        remove: "yes"
-      });
-
-      expect(res.status).toBe(302);
-      expect(res.header.location).toBe(getUrl(REVIEW_LIMITED_PARTNERS_URL));
-
-      expect(appDevDependencies.limitedPartnerGateway.limitedPartners).toHaveLength(0);
-    });
-
-    it("should redirect to the review limited partners page - lp not removed", async () => {
-      setLocalesEnabled(true);
-
-      const limitedPartnerLegalEntity = new LimitedPartnerBuilder()
-        .isLegalEntity()
-        .withId(appDevDependencies.limitedPartnerGateway.limitedPartnerId)
-        .build();
-
-      appDevDependencies.limitedPartnerGateway.feedLimitedPartners([limitedPartnerLegalEntity]);
-
-      const res = await request(app).post(URL).send({
-        pageType: RegistrationPageType.removeLimitedPartner,
-        remove: "no"
-      });
-
-      expect(res.status).toBe(302);
-      expect(res.header.location).toBe(getUrl(REVIEW_LIMITED_PARTNERS_URL));
-
-      expect(appDevDependencies.limitedPartnerGateway.limitedPartners).toHaveLength(1);
-    });
-  });
+runRemoveLimitedPartnerTests({
+  url: REMOVE_LIMITED_PARTNER_URL,
+  pageType: {
+    removeLimitedPartner: TransitionPageType.removeLimitedPartner
+  },
+  redirectUrlReview: REVIEW_LIMITED_PARTNERS_URL,
+  translateExclude: [],
+  serviceTitleTranslationKey: SERVICE_NAME_KEY_TRANSITION
 });
