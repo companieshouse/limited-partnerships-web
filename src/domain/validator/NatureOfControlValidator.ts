21d135b627050f697d100e2fa6494ab9d2244073
vschot <vschot@companieshouse.gov.uk>
2026-09-11 15:42:27 +0100

    [LP-2266] : Update error messages on add NoC pages

--- src/domain/validator/NatureOfControlValidator.ts
+++ src/domain/validator/NatureOfControlValidator.ts
@@ -1,14 +1,24 @@
-import { NatureOfControl, NatureOfControlType } from "@companieshouse/api-sdk-node/dist/services/limited-partnerships";
+import {
+  NatureOfControl,
+  NatureOfControlType,
+  PersonWithSignificantControlType
+} from "@companieshouse/api-sdk-node/dist/services/limited-partnerships";
 import UIErrors from "../entities/UIErrors";
 
 export type NatureOfControlUIFields = NatureOfControl & { share_of_assets?: string; voting_rights?: string };
 class NatureOfControlValidator {
   private natureOfControl?: NatureOfControlUIFields;
+  private personWithSignificantControlType?: PersonWithSignificantControlType;
 
   private errorMessages: Record<string, any> = {};
 
-  set(natureOfControl: NatureOfControlUIFields, i18n: any): this {
+  set(
+    natureOfControl: NatureOfControlUIFields,
+    personWithSignificantControlType: PersonWithSignificantControlType,
+    i18n: any
+  ): this {
     this.natureOfControl = natureOfControl;
+    this.personWithSignificantControlType = personWithSignificantControlType;
     this.errorMessages = i18n?.errorMessages?.personWithSignificantControl?.addNatureOfControl || {};
     return this;
   }
@@ -50,7 +60,7 @@ class NatureOfControlValidator {
     }
 
     // If no share of assets or voting rights percentages are selected, then either right to appointment and remove or significant influence control must be selected
-    this.hasSelectAtLeastOne(uiErrors, this.natureOfControl?.type);
+    this.handleNoSelectionsIndividual(uiErrors);
   }
 
   validateFirm(uiErrors: UIErrors): void {
@@ -62,7 +72,7 @@ class NatureOfControlValidator {
       uiErrors.setWebError("voting_rights", this.errorMessages.firm.votingRightsMissing);
     }
 
-    this.hasSelectAtLeastOne(uiErrors, this.natureOfControl?.type);
+    this.handleNoSelections(uiErrors, this.natureOfControl?.type);
   }
 
   validateTrust(uiErrors: UIErrors): void {
@@ -74,7 +84,7 @@ class NatureOfControlValidator {
       uiErrors.setWebError("voting_rights", this.errorMessages.trust.votingRightsMissing);
     }
 
-    this.hasSelectAtLeastOne(uiErrors, this.natureOfControl?.type);
+    this.handleNoSelections(uiErrors, this.natureOfControl?.type);
   }
 
   private hasShareOfAssetsPercentage() {
@@ -101,17 +111,36 @@ class NatureOfControlValidator {
     return this.hasVotingRightsPercentage() || this.natureOfControl?.voting_rights_does_not_apply;
   }
 
-  private hasSelectAtLeastOne(uiErrors: UIErrors, type?: string) {
+  private handleNoSelectionsIndividual(uiErrors: UIErrors) {
+    if (this.hasNoSelections()) {
+      const pscTypeMapSelection: Map<PersonWithSignificantControlType, string> = new Map([
+        [PersonWithSignificantControlType.INDIVIDUAL_PERSON, this.errorMessages.individual.youMustSelectAtLeastOne.individual],
+        [PersonWithSignificantControlType.RELEVANT_LEGAL_ENTITY, this.errorMessages.individual.youMustSelectAtLeastOne.rle],
+        [PersonWithSignificantControlType.OTHER_REGISTRABLE_PERSON, this.errorMessages.individual.youMustSelectAtLeastOne.orp]
+      ]);
+
+      const personWithSignificantControlType = this.personWithSignificantControlType as PersonWithSignificantControlType;
+      const errorMessage = pscTypeMapSelection.get(personWithSignificantControlType) ?? "";
+
+      uiErrors.setWebError("", errorMessage);
+    }
+  }
+
+  private handleNoSelections(uiErrors: UIErrors, type?: string) {
     const typeToLowerCase = type?.toLocaleLowerCase() ?? "";
 
-    if (
+    if (this.hasNoSelections()) {
+      uiErrors.setWebError("", this.errorMessages[typeToLowerCase].youMustSelectAtLeastOne);
+    }
+  }
+
+  private hasNoSelections() {
+    return (
       !this.hasShareOfAssetsPercentage() &&
       !this.hasVotingRightsPercentage() &&
       !this.natureOfControl?.right_to_appointment_and_remove &&
       !this.natureOfControl?.significant_influence_control
-    ) {
-      uiErrors.setWebError("", this.errorMessages[typeToLowerCase].youMustSelectAtLeastOne);
-    }
+    );
   }
 }
 
