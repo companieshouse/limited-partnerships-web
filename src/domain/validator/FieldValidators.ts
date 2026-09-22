d9f3a5759450a1d49da505d31ab4b9db4f7b3a6c
markpit <11330756+markpit@users.noreply.github.com>
2026-09-22 16:11:36 +0100

    LP-2292 convert field validator functions to arrow functions

--- src/domain/validator/FieldValidators.ts
+++ src/domain/validator/FieldValidators.ts
@@ -1,8 +1,7 @@
 import UIErrors from "../entities/UIErrors";
 import { VALID_CHARACTERS_REGEX } from "../../config/constants";
 
-// TODO arrow functions
-export function containsInvalidCharacters(fieldValue: string | undefined, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean {
+export const containsInvalidCharacters = (fieldValue: string | undefined, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean => {
   if (fieldValue && !VALID_CHARACTERS_REGEX.test(fieldValue)) {
     uiErrors.setWebError(fieldName, errorMessage);
     return true;
@@ -10,23 +9,23 @@ export function containsInvalidCharacters(fieldValue: string | undefined, fieldN
   return false;
 };
 
-export function isFieldValueMissing(fieldValue: string | undefined, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean {
+export const isFieldValueMissing = (fieldValue: string | undefined, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean => {
   if (typeof fieldValue === "string" ? !fieldValue?.trim() : (fieldValue === undefined || fieldValue === null)) {
     uiErrors.setWebError(fieldName, errorMessage);
     return true;
   }
   return false;
-}
+};
 
-export function isFieldValueTooLong(fieldValue: string | undefined, maxLength: number, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean {
+export const isFieldValueTooLong = (fieldValue: string | undefined, maxLength: number, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean => {
   if (fieldValue?.trim().length && fieldValue?.trim().length > maxLength) {
     uiErrors.setWebError(fieldName, errorMessage);
     return true;
   }
   return false;
-}
+};
 
-export function validateField(fieldValue: string | undefined, fieldName: string, maxLength: number, uiErrors: UIErrors, errorMessages: Record<string, string>) {
+export const validateField = (fieldValue: string | undefined, fieldName: string, maxLength: number, uiErrors: UIErrors, errorMessages: Record<string, string>) => {
   const { missingMessage, invalidMessage, tooLongMessage } = errorMessages;
 
   if (isFieldValueMissing(fieldValue, fieldName, uiErrors, missingMessage)) {
@@ -40,4 +39,4 @@ export function validateField(fieldValue: string | undefined, fieldName: string,
   if (isFieldValueTooLong(fieldValue, maxLength, fieldName, uiErrors, tooLongMessage)) {
     return;
   }
-}
+};
