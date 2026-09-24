808591a61e0b3fa06ae945e4bfaec31d66bb0404
markpit <11330756+markpit@users.noreply.github.com>
2026-09-24 10:57:12 +0100

    LP-2292 Use default for string param if missing in field validator functions

--- src/domain/validator/FieldValidators.ts
+++ src/domain/validator/FieldValidators.ts
@@ -1,7 +1,7 @@
 import UIErrors from "../entities/UIErrors";
 import { VALID_CHARACTERS_REGEX } from "../../config/constants";
 
-export const containsInvalidCharacters = (fieldValue: string | undefined, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean => {
+export const containsInvalidCharacters = (fieldValue: string = "", fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean => {
   if (fieldValue && !VALID_CHARACTERS_REGEX.test(fieldValue)) {
     uiErrors.setWebError(fieldName, errorMessage);
     return true;
@@ -9,7 +9,7 @@ export const containsInvalidCharacters = (fieldValue: string | undefined, fieldN
   return false;
 };
 
-export const isFieldValueMissing = (fieldValue: string | undefined, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean => {
+export const isFieldValueMissing = (fieldValue: string = "", fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean => {
   if (typeof fieldValue === "string" ? !fieldValue?.trim() : (fieldValue === undefined || fieldValue === null)) {
     uiErrors.setWebError(fieldName, errorMessage);
     return true;
@@ -17,7 +17,7 @@ export const isFieldValueMissing = (fieldValue: string | undefined, fieldName: s
   return false;
 };
 
-export const isFieldValueTooLong = (fieldValue: string | undefined, maxLength: number, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean => {
+export const isFieldValueTooLong = (fieldValue: string = "", maxLength: number, fieldName: string, uiErrors: UIErrors, errorMessage: string): boolean => {
   if (fieldValue?.trim().length && fieldValue?.trim().length > maxLength) {
     uiErrors.setWebError(fieldName, errorMessage);
     return true;
@@ -25,7 +25,7 @@ export const isFieldValueTooLong = (fieldValue: string | undefined, maxLength: n
   return false;
 };
 
-export const validateField = (fieldValue: string | undefined, fieldName: string, maxLength: number, uiErrors: UIErrors, errorMessages: Record<string, string>) => {
+export const validateField = (fieldValue: string = "", fieldName: string, maxLength: number, uiErrors: UIErrors, errorMessages: Record<string, string>) => {
   const { missingMessage, invalidMessage, tooLongMessage } = errorMessages;
 
   if (isFieldValueMissing(fieldValue, fieldName, uiErrors, missingMessage)) {
